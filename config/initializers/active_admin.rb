e70ef0e867d6cb5a7e41048fa6dcf3722f2623af
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2014-07-09 22:42:52 +0200

    Config ActiveAdmin

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -5,57 +5,19 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Admin Ragedevert Ch"
-
-  # Set the link url for the title. For example, to take
-  # users to your main site. Defaults to no link.
-  #
-  # config.site_title_link = "/"
+  config.site_title = "RageDeVert Admin"
+  config.root_to = 'dashboard#index'
 
   # Set an optional image to be displayed for the header
   # instead of a string (overrides :site_title)
   #
   # Note: Aim for an image that's 21px high so it fits in the header.
   #
-  # config.site_title_image = "logo.png"
+  # config.site_title_image = 'logo.png'
 
-  # == Default Namespace
-  #
-  # Set the default namespace each administration resource
-  # will be added to.
-  #
-  # eg:
-  #   config.default_namespace = :hello_world
-  #
-  # This will create resources in the HelloWorld module and
-  # will namespace routes to /hello_world/*
-  #
-  # To set no namespace by default, use:
-  #   config.default_namespace = false
-  #
-  # Default:
-  # config.default_namespace = :admin
-  #
-  # You can customize the settings for each namespace by using
-  # a namespace block. For example, to change the site title
-  # within a namespace:
-  #
-  #   config.namespace :admin do |admin|
-  #     admin.site_title = "Custom Admin Title"
-  #   end
-  #
-  # This will ONLY change the title for the admin section. Other
-  # namespaces will continue to use the main "site_title" configuration.
-
-  # == User Authentication
-  #
-  # Active Admin will automatically call an authentication
-  # method in a before filter of all controller actions to
-  # ensure that there is a currently logged in admin user.
-  #
-  # This setting changes the method which Active Admin calls
-  # within the application controller.
+  config.default_namespace = false
   config.authentication_method = :authenticate_admin!
+  config.current_user_method = :current_admin
 
   # == User Authorization
   #
@@ -74,16 +36,6 @@ ActiveAdmin.setup do |config|
   # doesn't have access to Dashboard, he'll end up in a redirect loop.
   # Method provided here should be defined in application_controller.rb.
   # config.on_unauthorized_access = :access_denied
-
-  # == Current User
-  #
-  # Active Admin will associate actions with the current
-  # user performing them.
-  #
-  # This setting changes the method which Active Admin calls
-  # (within the application controller) to return the currently logged in user.
-  config.current_user_method = :current_admin
-
 
   # == Logging Out
   #
@@ -104,24 +56,15 @@ ActiveAdmin.setup do |config|
   # config.logout_link_method = :get
 
 
-  # == Root
-  #
-  # Set the action to call for the root path. You can set different
-  # roots for each namespace.
-  #
-  # Default:
-  # config.root_to = 'dashboard#index'
-
-
   # == Admin Comments
   #
   # This allows your users to comment on any resource registered with Active Admin.
   #
   # You can completely disable comments:
-  # config.allow_comments = false
+  config.allow_comments = false
   #
   # You can disable the menu item for the comments index page:
-  # config.show_comments_in_menu = false
+  config.show_comments_in_menu = false
   #
   # You can change the name under which comments are registered:
   # config.comments_registration_name = 'AdminComment'
@@ -131,7 +74,7 @@ ActiveAdmin.setup do |config|
   #
   # Enable and disable Batch Actions
   #
-  config.batch_actions = true
+  config.batch_actions = false
 
 
   # == Controller Filters
@@ -152,7 +95,7 @@ ActiveAdmin.setup do |config|
   # Breadcrumbs are enabled by default. You can customize them for individual
   # resources or you can disable them globally from here.
   #
-  # config.breadcrumb = false
+  config.breadcrumb = false
 
 
   # == Register Stylesheets & Javascripts
@@ -201,7 +144,6 @@ ActiveAdmin.setup do |config|
   #     end
   #   end
 
-
   # == Download Links
   #
   # You can disable download links on resource listing pages,
@@ -223,14 +165,12 @@ ActiveAdmin.setup do |config|
   #
   #   end
 
-
   # == Pagination
   #
   # Pagination is enabled by default for all resources.
   # You can control the default per page count for all resources here.
   #
   # config.default_per_page = 30
-
 
   # == Filters
   #
@@ -239,5 +179,12 @@ ActiveAdmin.setup do |config|
   # You can enable or disable them for all resources here.
   #
   # config.filters = true
-
 end
+
+class MyFooter < ActiveAdmin::Component
+  def build
+    super(id: 'footer')
+    para ''
+  end
+end
+ActiveAdmin.setup { |config| config.view_factory.footer = MyFooter }
