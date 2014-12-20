3d5a5c27d8d2fb75b3f8bc7de1a9371480ca394f
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2014-12-20 14:55:25 +0100

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  constraints subdomain: 'admin' do
+    devise_for :admins, ActiveAdmin::Devise.config
+    ActiveAdmin.routes(self)
+    get 'halfday_works/calendar' => 'halfday_works_calendar#show'
+  end
+
   scope module: 'members',  as: 'members' do
     constraints subdomain: 'membres' do
       get '/' => redirect('/token/recover')
@@ -12,14 +18,5 @@ Rails.application.routes.draw do
         post :recover, on: :member
       end
     end
-  end
-
-  constraints subdomain: 'admin' do
-    devise_for :admins, ActiveAdmin::Devise.config
-    ActiveAdmin.routes(self)
-  end
-
-  constraints subdomain: 'admin' do
-    get 'halfday_works/calendar' => 'halfday_works_calendar#show'
   end
 end
