fce38595cdbaa01ab16792bd1a6051438862696b
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2014-12-26 13:16:39 +0100

    Fix Membership form

--- app/admin/membership.rb
+++ app/admin/membership.rb
@@ -69,7 +69,7 @@ ActiveAdmin.register Membership do
   controller do
     def build_resource
       super
-      resource.started_on = Date.today_2015.beginning_of_year.max
+      resource.started_on = Date.today_2015.beginning_of_year
       resource.ended_on = Date.today_2015.end_of_year
       resource
     end
