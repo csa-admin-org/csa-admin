d357ac7834a380b234b7e8230075c306b22fe28a
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2014-12-29 15:41:15 +0100

    Update Ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,16 +7,12 @@ class Ability
     else
       cannot [:manage, :read], Admin
       can [:manage, :read], Admin, id: admin.id
-      cannot :manage, [Basket, Distribution, Delivery]
+      cannot :manage, [Basket, Delivery]
       can :create, [Member, Membership, Distribution]
+      can :update, [Member, Distribution]
       can :destroy, [Member, Membership], can_destroy?: true
       can :update, Membership, can_update?: true
       can :read, :all
-      if admin.email == 'chantalgraef@gmail.com'
-        cannot :read, HalfdayWork
-      else
-        can :update, Distribution
-      end
     end
   end
 end
