2cb795712242f12e084f3bf7d3477d51a1143ccf
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2015-01-02 11:44:50 +0100

    Permit membership note params

--- app/admin/membership.rb
+++ app/admin/membership.rb
@@ -82,7 +82,7 @@ ActiveAdmin.register Membership do
   permit_params *%i[
     member_id billing_member_id basket_id distribution_id
     annual_price annual_halfday_works
-    started_on ended_on
+    started_on ended_on note
   ]
 
   controller do
