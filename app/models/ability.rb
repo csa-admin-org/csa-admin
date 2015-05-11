b7d582eaa6cde4f2f4d135440136e83451d3bf21
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2015-05-11 20:20:16 +0200

    Refactoring

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,14 +2,14 @@ class Ability
   include CanCan::Ability
 
   def initialize(admin)
+    cannot :manage, [Invoice]
     if admin.email == 'thibaud@thibaud.gg'
       can :manage, :all
-      cannot :manage, [Invoice]
       can :read, :all
     else
       cannot [:manage, :read], Admin
       can [:manage, :read], Admin, id: admin.id
-      cannot :manage, [Invoice, Basket, Delivery]
+      cannot :manage, [Basket, Delivery]
       can :manage, [HalfdayWork, Absence]
       can :create, [Member, Membership, Distribution]
       can :update, [Member, Distribution]
