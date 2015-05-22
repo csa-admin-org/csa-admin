16c16d41952495638eb72e2f93ec461b9a847052
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2015-05-22 11:28:10 +0200

    Fix Ability for HalfdayWorkDate

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,20 +2,19 @@ class Ability
   include CanCan::Ability
 
   def initialize(admin)
-    cannot :manage, [Invoice]
+    can :read, :all
     if admin.email == 'thibaud@thibaud.gg'
       can :manage, :all
-      can :read, :all
     else
       cannot [:manage, :read], Admin
       can [:manage, :read], Admin, id: admin.id
       cannot :manage, [Basket, Delivery]
-      can :manage, [HalfdayWork, Absence]
+      can :manage, [HalfdayWork, HalfdayWorkDate, Absence]
       can :create, [Member, Membership, Distribution]
       can :update, [Member, Distribution]
       can :destroy, [Member, Membership], can_destroy?: true
       can :update, Membership, can_update?: true
-      can :read, :all
     end
+    cannot :manage, [Invoice]
   end
 end
