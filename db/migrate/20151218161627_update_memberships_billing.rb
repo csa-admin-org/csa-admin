a72a5b96f63ef58fb34c5c6cb26c23dc586f2155
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2015-12-22 20:44:23 +0100

    Clear migrations

--- db/migrate/20151218161627_update_memberships_billing.rb
+++ db/migrate/20151218161627_update_memberships_billing.rb
@@ -6,18 +6,5 @@ class UpdateMembershipsBilling < ActiveRecord::Migration
       .where(annual_halfday_works: nil)
       .update_all(annual_halfday_works: HalfdayWork::MEMBER_PER_YEAR)
     rename_column :memberships, :annual_price, :halfday_works_annual_price
-
-    # To run once
-    # Membership.all.each do |m|
-    #   halfday_works_annual_price =
-    #     if m.annual_halfday_works == 0
-    #       HalfdayWork::MEMBER_PER_YEAR * HalfdayWork::PRICE
-    #     elsif m.halfday_works_annual_price && m.basket.annual_price > m.halfday_works_annual_price
-    #       m.halfday_works_annual_price - m.basket.annual_price
-    #     else
-    #       nil
-    #     end
-    #   m.update!(halfday_works_annual_price: halfday_works_annual_price)
-    # end
   end
 end
