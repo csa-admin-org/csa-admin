d2a2f6c2f37cd8ed09e1c8ee0960a0ff686c567f
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2017-12-28 11:58:31 +0100

    Allow admin to manage ActiveAdmin::Comment

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Ability do
   let(:ability) { Ability.new(admin) }
 
-  context 'superadmin admin' do
+  context 'superadmin rights' do
     let(:admin) { create(:admin, rights: 'superadmin') }
 
     specify { expect(ability.can?(:create, Member)).to be true }
@@ -14,7 +14,7 @@ describe Ability do
     specify { expect(ability.can?(:put_back_to_waiting_list, Member)).to be true }
   end
 
-  context 'admin admin' do
+  context 'admin rights' do
     let(:admin) { create(:admin, rights: 'admin') }
 
     specify { expect(ability.can?(:create, Member)).to be true }
@@ -23,5 +23,19 @@ describe Ability do
     specify { expect(ability.can?(:validate, Member)).to be true }
     specify { expect(ability.can?(:remove_from_waiting_list, Member)).to be true }
     specify { expect(ability.can?(:put_back_to_waiting_list, Member)).to be true }
+    specify { expect(ability.can?(:manage, ActiveAdmin::Comment)).to be true }
+  end
+
+  context 'standard rights' do
+    let(:admin) { create(:admin, rights: 'standard') }
+
+    specify { expect(ability.can?(:manage, Member)).to be false }
+    specify { expect(ability.can?(:read, Member)).to be true }
+    specify { expect(ability.can?(:destroy, Member)).to be false }
+    specify { expect(ability.can?(:validate, Member)).to be false }
+    specify { expect(ability.can?(:remove_from_waiting_list, Member)).to be false }
+    specify { expect(ability.can?(:put_back_to_waiting_list, Member)).to be false }
+    specify { expect(ability.can?(:manage, ActiveAdmin::Comment)).to be false }
+    specify { expect(ability.can?(:create, ActiveAdmin::Comment)).to be true }
   end
 end
