dad8db31b3d708ad49fc4979c7120312e1a730a1
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2018-01-12 11:28:05 +0100

    Fix ExceptionNotification email settings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :prepare_exception_notifier
 
   def access_denied(exception)
     redirect_to root_path, alert: exception.message
+  end
+
+  private
+
+  def prepare_exception_notifier
+    request.env['exception_notifier.exception_data'] = {
+      current_acp: Apartment::Tenant.current
+    }
   end
 end
