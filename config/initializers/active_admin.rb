8f4654f22feee048bfc6516065e0a8365aa5cd9d
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2018-06-07 21:17:48 +0200

    Fix BasketContent admin small/big name display

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -194,6 +194,7 @@ class ActiveAdmin::ResourceController
   include ApplicationHelper
   include AcpsHelper
   include FormsHelper
+  include BasketContentsHelper
 
   def csv_filename
     "#{resource_class.model_name.human(count: 2).downcase.dasherize.delete(' ')}-#{Time.zone.now.to_date.to_s(:default)}.csv"
