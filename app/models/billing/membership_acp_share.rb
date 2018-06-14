6b69586ba67398a70368c889361c747b9cbd9117
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2018-06-14 21:08:32 +0200

    Add Billing::MembershipACPShare
    This class allows to automatically invoice ACP Share for member starting a new membership.

---
+++ app/models/billing/membership_acp_share.rb
@@ -0,0 +1,34 @@
+module Billing
+  class MembershipACPShare
+    attr_reader :member
+
+    def self.invoice!(member, **attrs)
+      new(member).invoice!(attrs)
+    end
+
+    def initialize(member)
+      @member = member
+    end
+
+    def invoice!(**attrs)
+      return unless billable?
+
+      attrs[:date] = Date.current
+      attrs[:acp_shares_number] = missing_acp_shares_number
+      member.invoices.create!(attrs)
+    end
+
+    def billable?
+      membership && missing_acp_shares_number.positive?
+    end
+
+    def missing_acp_shares_number
+      @missing_acp_shares_number ||=
+        membership.basket_size.acp_shares_number - member.acp_shares_number
+    end
+
+    def membership
+      @membership ||= member.memberships.ongoing.first
+    end
+  end
+end
