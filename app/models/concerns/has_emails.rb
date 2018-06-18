a4d1f8de0e5bbc1c393cda79f1006987a559b1f6
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2018-06-18 21:05:55 +0200

    Ensure email format and uniqueness on Member
    This will be even more important with the introduction of more secure passwordless session.

--- app/models/concerns/has_emails.rb
+++ app/models/concerns/has_emails.rb
@@ -1,11 +1,17 @@
 module HasEmails
   extend ActiveSupport::Concern
 
+  EMAIL_REGEXP = /\A[^@\s]+@[^@\s]+\z/
+
   included do
     attr_accessor :email
 
+    validate :emails_must_be_valid
+
     scope :with_emails, -> { where.not(emails: ['', nil]) }
-    scope :with_email, ->(email) { where('emails ILIKE ?', "%#{email}%") }
+    scope :with_email, ->(email) {
+      where("lower(emails) ~ ('(^|,\s)' || lower(?) || '(,\s|$)')", email)
+    }
   end
 
   def emails=(emails)
@@ -21,6 +27,15 @@ module HasEmails
   end
 
   private
+
+  def emails_must_be_valid
+    emails_array.each do |email|
+      unless email.match?(EMAIL_REGEXP)
+        errors.add(:emails, :invalid)
+        break
+      end
+    end
+  end
 
   def string_to_a(str)
     str.to_s.split(',').each(&:strip!)
