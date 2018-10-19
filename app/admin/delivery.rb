7868fb9f05f0d809baacd36c464e313dc095b3b5
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2018-10-19 11:52:26 +0200

    Improve delivery admin show page
    This patch improve the delivery show page with the same details shown on 
    the dashboard, this makes possible to see past/future delivery details.
    
    This also adds a new CSV download for deliveries listing all baskets 
    details.

--- app/admin/delivery.rb
+++ app/admin/delivery.rb
@@ -7,7 +7,7 @@ ActiveAdmin.register Delivery do
 
   # Workaround for ActionController::UnknownFormat (xlsx download)
   # https://github.com/activeadmin/activeadmin/issues/4945#issuecomment-302729459
-  index download_links: -> { params[:action] == 'show' ? [:xlsx, :pdf] : false } do
+  index download_links: -> { params[:action] == 'show' ? [:xlsx, :pdf] : [:csv] } do
     column '#', ->(delivery) { auto_link delivery, delivery.number }
     column :date, ->(delivery) { auto_link delivery, l(delivery.date) }
     column :note, ->(delivery) { truncate delivery.note, length: 175 }
@@ -17,20 +17,113 @@ ActiveAdmin.register Delivery do
     end
   end
 
-  show do |delivery|
-    attributes_table do
-      row('#') { delivery.number }
-      row(:date) { l delivery.date }
-      row(:note) { text_format(delivery.note) }
+  csv do
+    column(:id)
+    column(:date)
+    column(:baskets) { |d| d.basket_counts.sum(&:count) }
+
+    Distribution.all.each do |distribution|
+      column(distribution.name) { |d| BasketCounts.new(d, distribution.id).sum }
+    end
+
+    BasketSize.all.each do |basket_size|
+      column(basket_size.name) { |d| d.basket_counts.sum_basket_size(basket_size.id) }
+    end
+
+    if BasketComplement.any?
+      BasketComplement.all.each do |basket_complement|
+        column(basket_complement.name) { |d| BasketComplementCount.new(basket_complement, d).count }
+      end
     end
   end
 
-  action_item :xlsx, only: :show do
-    link_to Delivery.human_attribute_name(:xlsx_recap), delivery_path(resource, format: :xlsx)
-  end
+  show do |delivery|
+    columns do
+      column do
+        panel Basket.model_name.human(count: 2) do
+          counts = delivery.basket_counts
+          if counts.present?
+            table_for counts.all do
+              column Distribution.model_name.human, :title
+              column Basket.model_name.human, :count, class: 'align-right'
+              column "#{BasketSize.all.map(&:name).join(' /&nbsp;')}".html_safe, :baskets_count, class: 'align-right'
+            end
 
-  action_item :pdf, only: :show do
-    link_to Delivery.human_attribute_name(:signature_sheets), delivery_path(resource, format: :pdf)
+            if Distribution.paid.any?
+              free_distributions = Distribution.free
+              paid_distributions = Distribution.paid
+              free_counts = BasketCounts.new(delivery, free_distributions.pluck(:id))
+              paid_counts = BasketCounts.new(delivery, paid_distributions.pluck(:id))
+              totals = [
+                OpenStruct.new(
+                  title: "#{Basket.model_name.human(count: 2)}: #{free_distributions.pluck(:name).to_sentence}",
+                  count: t('.total', number: free_counts.sum),
+                  baskets_count: t('.totals', numbers: free_counts.sum_detail)),
+                OpenStruct.new(
+                  title: t('.baskets_to_prepare'),
+                  count: t('.total', number: paid_counts.sum),
+                  baskets_count: t('.totals', numbers: paid_counts.sum_detail))
+              ]
+              table_for totals do
+                column nil, :title
+                column nil, :count, class: 'align-right'
+                column nil, :baskets_count, class: 'align-right'
+              end
+            end
+
+            table_for nil do
+              column nil, :title
+              column(class: 'align-right') { "Total: #{counts.sum}" }
+              column(class: 'align-right') { t('.totals', numbers: counts.sum_detail) }
+            end
+
+            if BasketComplement.any?
+              counts = BasketComplementCount.all(delivery)
+              div id: 'basket-complements-table' do
+                if counts.any?
+                  table_for counts do
+                    column BasketComplement.model_name.human, :title
+                    column t('.total', number: ''), :count, class: 'align-right'
+                  end
+                else
+                  em t('.no_basket_complements')
+                end
+              end
+            end
+
+            span do
+              link_to Delivery.human_attribute_name(:xlsx_recap), delivery_path(delivery, format: :xlsx)
+            end
+            span { '&nbsp;/&nbsp;'.html_safe }
+            span do
+              link_to Delivery.human_attribute_name(:signature_sheets), delivery_path(delivery, format: :pdf)
+            end
+          end
+        end
+      end
+
+      column do
+        attributes_table do
+          row('#') { delivery.number }
+          row(:date) { l delivery.date }
+        end
+
+        panel link_to(Absence.model_name.human(count: 2), absences_path(q: { including_date: delivery.date }, scope: :all)) do
+          absences = Absence.including_date(delivery.date).includes(:member)
+          if absences.any?
+            absences.map { |a| auto_link a.member }.join(', ').html_safe
+          else
+            content_tag :span, t('active_admin.empty'), class: 'empty'
+          end
+        end
+
+        attributes_table title: t('.notes') do
+          row(:note) { text_format(delivery.note) }
+        end
+
+        active_admin_comments
+      end
+    end
   end
 
   form do |f|
