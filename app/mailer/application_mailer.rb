d9e9c1d0bc75c205623fc23dbc6fd9aba36630b7
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2018-11-10 14:34:47 +0100

    Swich Devise mailer to Postmark
    This patch ensures that the "reset_password_instruction" is also using 
    Postmark custom templates instead of the default ActionMailer from 
    Devise/Rails.

--- app/mailer/application_mailer.rb
+++
@@ -1,9 +0,0 @@
-class ApplicationMailer < ActionMailer::Base
-  default from: %{"ACP Admin" <acp-admin@thibaud.gg>}
-
-  private
-
-  def default_url_options
-    { host: Current.acp.email_default_host }
-  end
-end
