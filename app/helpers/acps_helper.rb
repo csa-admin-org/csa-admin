76112d9ad6e1dbbc0f361ec9f41b23cd3d5f1c3c
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2019-06-07 14:50:35 +0200

    Member new form, remove fiscal year
    Remove the year from the fiscal year range displayed and only keep the full month instead.
    
    As the form could be open before the fiscal year debuts for a new ACP, showing the year is confusing.

--- app/helpers/acps_helper.rb
+++ app/helpers/acps_helper.rb
@@ -3,21 +3,11 @@ module AcpsHelper
     Current.acp.feature?(feature)
   end
 
-  def fiscal_year_range
-    fy = Current.acp.current_fiscal_year
-    range =
-      if fy.range.min.year == fy.range.max.year
-        [
-          l(fy.range.min, format: '%b'),
-          l(fy.range.max, format: '%b %Y')
-        ]
-      else
-        [
-          l(fy.range.min, format: '%b %Y'),
-          l(fy.range.max, format: '%b %Y')
-        ]
-      end
-    range.join(' - ')
+  def fiscal_year_months_range
+    Current.acp.current_fiscal_year
+      .range.minmax
+      .map { |d| l(d, format: '%B') }
+      .join(' – ')
   end
 
   def link_to_acp_website
