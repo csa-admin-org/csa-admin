2937bdaf16a2d8143e5e675555e1436c45fed880
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2019-11-26 08:38:24 +0100

    GroupBuying show producer website URL on member page

--- app/models/group_buying/producer.rb
+++ app/models/group_buying/producer.rb
@@ -7,6 +7,10 @@ module GroupBuying
     has_many :products, class_name: 'GroupBuying::Product'
 
     validates :name, presence: true
+    validates :website_url, format: {
+      with: %r{\Ahttps?://.*},
+      allow_blank: true
+    }
 
     def can_destroy?
       products.none?
