b76d78b1ad2f9ce194f2668143229b3032f25544
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2020-02-02 10:08:57 +0100

    Member page, show future membership deliveries

--- app/controllers/members/deliveries_controller.rb
+++ app/controllers/members/deliveries_controller.rb
@@ -2,9 +2,21 @@ class Members::DeliveriesController < Members::BaseController
   # GET /deliveries
   def index
     if @next_basket = current_member.next_basket
-      baskets = @next_basket.membership.baskets.includes(:delivery, :basket_size, :depot)
-      @future_baskets = baskets.coming.where.not(id: @next_basket.id)
-      @past_baskets = baskets.delivered
+      membership_ids = [@next_basket.membership_id]
+      if current_member.future_membership
+        membership_ids << current_member.future_membership.id
+      end
+      @future_baskets =
+        Basket
+          .where(membership_id: membership_ids)
+          .where.not(id: @next_basket.id)
+          .coming
+          .includes(:delivery, :basket_size, :depot)
+      @past_baskets =
+        @next_basket
+          .membership.baskets
+          .delivered
+          .includes(:delivery, :basket_size, :depot)
     end
   end
 end
