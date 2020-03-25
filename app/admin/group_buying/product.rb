ad3f26b953df5c32ab2a7bdacd07a73cde6d0eb2
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2020-03-25 21:15:56 +0100

    GroupBuying, batch product availability change
    This patch allows to change group buying product availability in batches.

--- app/admin/group_buying/product.rb
+++ app/admin/group_buying/product.rb
@@ -11,6 +11,7 @@ ActiveAdmin.register GroupBuying::Product do
   includes :producer, :order_items
 
   index do
+    selectable_column
     column :name, ->(product) { auto_link product }, sortable: :names
     column :available, ->(product) { status_tag(product.available? ? :yes : :no) }
     column :price, ->(product) { number_to_currency(product.price) }
@@ -48,9 +49,21 @@ ActiveAdmin.register GroupBuying::Product do
     names: I18n.available_locales,
     descriptions: I18n.available_locales)
 
+  batch_action :make_available do |selection|
+    GroupBuying::Product.where(id: selection).update_all(available: true)
+    redirect_back fallback_location: collection_path
+  end
+
+  batch_action :make_unavailable do |selection|
+    GroupBuying::Product.where(id: selection).update_all(available: false)
+    redirect_back fallback_location: collection_path
+  end
+
   controller do
     include TranslatedCSVFilename
   end
 
   config.sort_order = 'names_desc'
+  config.per_page = 50
+  config.batch_actions = true
 end
