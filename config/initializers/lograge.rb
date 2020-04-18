54a27965a903c6903fe6283dbbe73ed747497e99
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2020-04-18 15:50:37 +0200

    Add lograge logging
    Try to switch to one line logging into production for better Sumologic parsing.

---
+++ config/initializers/lograge.rb
@@ -0,0 +1,21 @@
+Rails.application.configure do
+  config.lograge.enabled = true
+
+  config.lograge.base_controller_class = [
+    'ActionController::Base',
+    'ActiveAdmin::BaseController'
+  ]
+
+  config.lograge.custom_payload do |controller|
+    payload ={
+      host: controller.request.host
+    }
+    if controller.respond_to?(:current_admin, true) && controller.send(:current_admin)
+      payload[:admin_id] = controller.send(:current_admin)&.id
+    end
+    if controller.respond_to?(:current_member, true) && controller.send(:current_member)
+      payload[:member_id] = controller.send(:current_member)&.id
+    end
+    payload
+  end
+end
