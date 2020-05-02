7406bf9d9aec71059788c0ea75d030964f4e28aa
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2020-05-02 21:40:21 +0200

    BasketContent, ensure good same_basket_quantities default label class
    This patch ensures that when a basket content is edited, the `same_basket_quantities`
    label class is properly set to with the `disabled` class if only one of the
    basket size is selected.

--- config/initializers/formtastic.rb
+++ config/initializers/formtastic.rb
@@ -111,3 +111,12 @@ Formtastic::FormBuilder.action_class_finder = Formtastic::ActionClassFinder
 
 # Which columns to skip when automatically rendering a form without any fields specified.
 # Formtastic::FormBuilder.skipped_columns = [:created_at, :updated_at, :created_on, :updated_on, :lock_version, :version]
+
+Formtastic::Inputs::Base::Labelling.module_eval do
+  def label_html_options
+    {
+      for: input_html_options[:id],
+      class: Array(options[:label_class])
+    }
+  end
+end
