3547160765717b0adec7f4c05404d7fbdcff6105
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2020-06-15 21:01:04 +0200

    Upgrade webpacker and js dependencies

--- app/javascript/packs/members.js
+++ app/javascript/packs/members.js
@@ -3,12 +3,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
+require('@rails/ujs').start()
+require('turbolinks').start()
 // require("@rails/activestorage").start()
 // require("channels")
 
-import 'core-js';
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
 
 import 'normalize.css';
 import 'scss/members.scss';
