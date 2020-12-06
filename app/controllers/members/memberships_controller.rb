347c54348225aed050f2f4b04366c452a3e6efa2
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2020-12-06 20:39:25 +0100

    Member page, fix accessing /membership page without membership

--- app/controllers/members/memberships_controller.rb
+++ app/controllers/members/memberships_controller.rb
@@ -2,8 +2,10 @@ class Members::MembershipsController < Members::BaseController
   # GET /membership
   def show
     @membership = current_member.current_or_future_membership
-    redirect_to members_member_path unless @membership
-
-    @membership.renewal_decision = :renew
+    if @membership
+      @membership.renewal_decision = :renew
+    else
+      redirect_to members_member_path
+    end
   end
 end
