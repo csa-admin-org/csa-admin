4d3ba88f6b11bdd5be41204e5da33daddebc8d39
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2020-12-20 14:04:15 +0100

    Rails 6.1 upgrade
    This patch upgrades Rails to 6.1 with the `rails app:update` command.

--- config/puma.rb
+++ config/puma.rb
@@ -8,9 +8,14 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
+# Specifies the `worker_timeout` threshold that Puma will use to wait before
+# terminating a worker in development environments.
+#
+worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
+
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -19,35 +24,20 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
-if ENV["WEB_CONCURRENCY"].to_i > 1
-  # Specifies the number of `workers` to boot in clustered mode.
-  # Workers are forked web server processes. If using threads and workers together
-  # the concurrency of the application would be max `threads` * `workers`.
-  # Workers do not work on JRuby or Windows (both of which do not support
-  # processes).
-  #
-  workers ENV["WEB_CONCURRENCY"]
+# Specifies the number of `workers` to boot in clustered mode.
+# Workers are forked web server processes. If using threads and workers together
+# the concurrency of the application would be max `threads` * `workers`.
+# Workers do not work on JRuby or Windows (both of which do not support
+# processes).
+#
+# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
-  # Use the `preload_app!` method when specifying a `workers` number.
-  # This directive tells Puma to first boot the application and load code
-  # before forking the application. This takes advantage of Copy On Write
-  # process behavior so workers use less memory. If you use this option
-  # you need to make sure to reconnect any threads in the `on_worker_boot`
-  # block.
-  #
-  preload_app!
-
-  # The code in the `on_worker_boot` will be called if you are using
-  # clustered mode by specifying a number of `workers`. After each worker
-  # process is booted this block will be run, if you are using `preload_app!`
-  # option you will want to use this block to reconnect to any threads
-  # or connections that may have been created at application boot, Ruby
-  # cannot share connections between processes.
-  #
-  on_worker_boot do
-    ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
-  end
-end
+# Use the `preload_app!` method when specifying a `workers` number.
+# This directive tells Puma to first boot the application and load code
+# before forking the application. This takes advantage of Copy On Write
+# process behavior so workers use less memory.
+#
+# preload_app!
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
--- spec/support/validation_matcher.rb
+++ spec/support/validation_matcher.rb
@@ -1,11 +1,11 @@
 RSpec::Matchers.define :have_valid do |*attributes|
   match do |record, _negated = false|
     record.validate
-    attributes.all? { |attribute| !record.errors.keys.include?(attribute) }
+    attributes.all? { |attribute| !record.errors.attribute_names.include?(attribute) }
   end
 
   match_when_negated do |record, _negated = false|
     record.validate
-    attributes.all? { |attribute| record.errors.keys.include?(attribute) }
+    attributes.all? { |attribute| record.errors.attribute_names.include?(attribute) }
   end
 end
