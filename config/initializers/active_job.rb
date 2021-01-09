d9dc22a561005d0a870cd807944cdad42a8c34d3
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2021-01-09 12:34:38 +0100

    Fix Initialization auto-loading issue
    This patch fixes the following deprecation warning:
    
    ```
    DEPRECATION WARNING: Initialization autoloaded the constant Apartmentable.
    
    Being able to do this is deprecated. Autoloading during initialization is going
    to be an error condition in future versions of Rails.
    
    Reloading does not reboot the application, and therefore code executed during
    initialization does not run again. So, if you reload Apartmentable, for example,
    the expected changes won't be reflected in that stale Module object.
    
    This autoloaded constant has been unloaded.
    
    In order to autoload safely at boot time, please wrap your code in a reloader
    callback this way:
    
        Rails.application.reloader.to_prepare do
          # Autoload classes and modules needed at boot time here.
        end
    
    That block runs when the application boots, and every time there is a reload.
    For historical reasons, it may run twice, so it has to be idempotent.
    
    Check the "Autoloading and Reloading Constants" guide to learn more about how
    Rails autoloads and reloads.
    ```

--- config/initializers/active_job.rb
+++ config/initializers/active_job.rb
@@ -1,6 +1,8 @@
-class ActiveJob::Base
-  # Don't use that when using inline jobs
-  unless Rails.env.test?
-    include Apartmentable
+Rails.application.reloader.to_prepare do
+  class ActiveJob::Base
+    # Don't use that when using inline jobs
+    unless Rails.env.test?
+      include Apartmentable
+    end
   end
 end
