15ee9bcad25d08baaaff42ef3c7c1d78bf4d8541
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2021-01-10 10:37:36 +0100

    Improve Billing XLSX perf
    Avoid N+1 queries.

--- app/models/xlsx/billing.rb
+++ app/models/xlsx/billing.rb
@@ -3,7 +3,18 @@ module XLSX
     def initialize(year)
       @invoices = Invoice.not_canceled.during_year(year)
       @payments = Payment.during_year(year)
-      @memberships = Membership.during_year(year)
+      @memberships = Membership.during_year(year).includes(:member)
+      @baskets =
+        Basket
+          .during_year(year)
+          .billable
+          .joins(membership: :member)
+          .merge(Member.no_salary_basket)
+      @memberships_basket_complements =
+        MembershipsBasketComplement
+          .joins(membership: :member)
+          .merge(Membership.during_year(year))
+          .merge(Member.no_salary_basket)
 
       build_worksheet(t('title'))
     end
@@ -25,14 +36,28 @@ module XLSX
         Invoice.human_attribute_name(:unit_price),
         Invoice.human_attribute_name(:total))
       BasketSize.all.each do |basket_size|
-        total = @memberships.sum { |m| m.basket_size_total_price(basket_size.id) }
+        total = @baskets.where(baskets: { basket_size_id: basket_size.id }).sum('baskets.quantity * baskets.basket_price')
         add_line("#{Basket.model_name.human}: #{basket_size.name}", total, basket_size.price)
+      end
+      if Current.acp.feature_flag?(:basket_price_extra)
+        total = @baskets.sum('baskets.quantity * memberships.basket_price_extra')
+        add_line("#{Membership.human_attribute_name(:basket_price_extra)}:", total)
       end
       add_empty_line
 
       if BasketComplement.any?
         BasketComplement.all.each do |basket_complement|
-          total = @memberships.sum { |m| m.basket_complement_total_price(basket_complement) }
+          total =
+            if basket_complement.annual_price_type?
+              @memberships_basket_complements
+                .where(memberships_basket_complements: { basket_complement: basket_complement })
+                .sum('memberships_basket_complements.quantity * memberships_basket_complements.price')
+            else
+              @baskets
+                .joins(:baskets_basket_complements)
+                .where(baskets_basket_complements: { basket_complement: basket_complement })
+                .sum('baskets_basket_complements.quantity * baskets_basket_complements.price')
+            end
           add_line("#{BasketComplement.model_name.human}: #{basket_complement.name}", total, basket_complement.price)
         end
         add_empty_line
@@ -40,20 +65,20 @@ module XLSX
 
       if Depot.paid.any?
         Depot.paid.each do |depot|
-          total = @memberships.sum { |m| m.depot_total_price(depot.id) }
+          total = @baskets.where(baskets: { depot_id: depot.id }).sum('baskets.quantity * baskets.depot_price')
           add_line("#{Depot.model_name.human}: #{depot.name}", total, depot.price)
         end
         add_empty_line
       end
 
-      add_line("#{t('adjustments')}: #{Basket.model_name.human(count: 2)}", @memberships.sum(&:baskets_annual_price_change))
+      add_line("#{t('adjustments')}: #{Basket.model_name.human(count: 2)}", @memberships.sum(:baskets_annual_price_change))
       if BasketComplement.any?
-        add_line("#{t('adjustments')}: #{BasketComplement.model_name.human}", @memberships.sum(&:basket_complements_annual_price_change))
+        add_line("#{t('adjustments')}: #{BasketComplement.model_name.human}", @memberships.sum(:basket_complements_annual_price_change))
       end
-      add_line("#{t('adjustments')}: #{ApplicationController.helpers.activities_human_name}", @memberships.sum(&:activity_participations_annual_price_change))
+      add_line("#{t('adjustments')}: #{ApplicationController.helpers.activities_human_name}", @memberships.sum(:activity_participations_annual_price_change))
 
       add_empty_line
-      add_line((t('memberships_total')), @memberships.sum(&:price))
+      add_line((t('memberships_total')), @memberships.sum(:price))
 
       add_empty_line
       add_empty_line
