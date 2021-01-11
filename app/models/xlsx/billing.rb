d68f4ba1f38e0aaf0aa7f9a92b4f614768ae8374
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2021-01-11 20:03:44 +0100

    XLSX::Billing improvement
    The manual payments total should only include positive payments as the next
    line is already reporting all the refunds. So if the manual payments total is
    already negative the payment sum total was no more correct.

--- app/models/xlsx/billing.rb
+++ app/models/xlsx/billing.rb
@@ -114,7 +114,7 @@ module XLSX
 
       t_payment = Payment.model_name.human(count: 2)
       add_line("#{t_payment}: #{t('isr')}", @payments.isr.sum(:amount))
-      add_line("#{t_payment}: #{t('manual')}", @payments.manual.sum(:amount))
+      add_line("#{t_payment}: #{t('manual')}", @payments.manual.where('amount > 0').sum(:amount))
       add_line("#{t_payment}: #{t('refund')}", @payments.refund.sum(:amount))
       add_line(t_payment, @payments.sum(:amount))
 
