ea470de2f56435d06354ce243f1eb87a26ae0f12
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2021-03-06 10:03:26 +0100

    Italian translations preparation
    This patch prepares the translations YAML files and email templates for
    welcoming Italian. To begin with, everything is copying the French
    locales so it easier to go file by file and replace existing strings by
    Italian ones.

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module ACPAdmin
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.i18n.available_locales = %w[en fr de]
+    config.i18n.available_locales = %w[en fr de it]
     config.i18n.default_locale = :fr
     config.i18n.fallbacks = true
 
