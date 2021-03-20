5847c297075dce6022ab9e4ba44898af0301d127
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2021-03-20 20:28:14 +0100

    Try to improve Sentry tags for performance

--- lib/acp_elevator.rb
+++ lib/acp_elevator.rb
@@ -3,6 +3,8 @@ require 'apartment/elevators/generic'
 class ACPElevator < Apartment::Elevators::Generic
   def parse_tenant_name(request)
     host = request.host.split('.')[-2]
-    ACP.find_by!(host: host).tenant_name
+    tenant_name = ACP.find_by!(host: host).tenant_name
+    Sentry.set_tags(acp: tenant_name)
+    tenant_name
   end
 end
