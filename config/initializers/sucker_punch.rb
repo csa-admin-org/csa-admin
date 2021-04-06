8e3e1af4e7034d9b9482e06780695f006dbb59ea
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2021-04-06 18:48:33 +0200

    Use built-in async queue adapter instead of SuckerPunch
    The `:async` queue adapter provides the same functionality than sucker_punch and
    hopefully will better handle the AR connection pool.
    
    This should be good enough even for production as we mostly send async email,
    aka really short-lived job.

--- config/initializers/sucker_punch.rb
+++
@@ -1,7 +0,0 @@
-SuckerPunch.exception_handler = -> (e, klass, args) {
-  ExceptionNotifier.notify_exception(e)
-  Sentry.capture_exception(e, extra: {
-    class_name: klass,
-    args: args
-  })
-}
