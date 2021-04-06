d8826662036414e4ee9f743edee0969481cb329b
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2021-04-06 19:20:08 +0200

    Mailchimp, sync using ACP default locale
    Required when the ACP doesn't support the app default locale (fr).

--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -3,9 +3,11 @@ module Newsletter
     return unless mailchimp_credentials = Current.acp.credentials(:mailchimp)
 
     mailchimp = Newsletter::MailChimp.new(mailchimp_credentials)
-    mailchimp.upsert_merge_fields
-    mailchimp.upsert_members(Member.all)
-    mailchimp.unsubscribe_deleted_members(Member.all)
+    I18n.with_locale(Current.acp.default_locale) do
+      mailchimp.upsert_merge_fields
+      mailchimp.upsert_members(Member.all)
+      mailchimp.unsubscribe_deleted_members(Member.all)
+    end
   rescue Gibbon::MailChimpError => e
     ExceptionNotifier.notify(e)
     Sentry.capture_exception(e)
