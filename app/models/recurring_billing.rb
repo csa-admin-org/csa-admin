852d18b561a00606ff3bc0d88e275d8e55464173
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2021-04-26 20:29:34 +0200

    Recurring billing next date fix
    This patch ensures that support member with no annual fee but only ACP share
    doesn't have any next date when the shares are paid.

--- app/models/recurring_billing.rb
+++ app/models/recurring_billing.rb
@@ -39,9 +39,9 @@ class RecurringBilling
           end
         n_date >= membership.fiscal_year.end_of_year ? date : n_date
       elsif member.support?
-        if annual_fee_billable?
+        if annual_fee_billable? || member.missing_acp_shares_number&.positive?
           next_billing_day
-        else
+        elsif Current.acp.annual_fee?
           next_billing_day(Current.fiscal_year.end_of_year + 1.day)
         end
       end
