3905786dd1f9f9787745e44882658d31d8d62398
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2021-06-05 21:40:53 +0200

    Lograge, set acp option

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   ]
 
   config.lograge.custom_payload do |controller|
-    payload ={
+    payload = {
       host: controller.request.host
     }
     if controller.respond_to?(:current_admin, true) && controller.send(:current_admin)
@@ -15,5 +15,13 @@ Rails.application.configure do
       payload[:member_id] = controller.send(:current_member)&.id
     end
     payload
+  end
+
+  config.lograge.custom_options = lambda do |event|
+    options = {}
+    unless Apartment::Tenant.current == 'public'
+      options[:acp] = Apartment::Tenant.current
+    end
+    options
   end
 end
