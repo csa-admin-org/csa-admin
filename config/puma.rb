97ceb22fb535a1333ec00bec084e20b26120cd8a
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2021-09-17 08:11:29 +0200

    Fix Puma config

--- config/puma.rb
+++ config/puma.rb
@@ -31,7 +31,7 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # processes).
 #
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
-workers { 0 }
+workers 0
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
