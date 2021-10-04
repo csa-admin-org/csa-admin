d32a2bc6b29b177e31dfa908ddfd91a748a22955
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2021-10-04 17:43:00 +0200

    Add cache-control to public and assets files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,9 +32,15 @@ Rails.application.configure do
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   config.require_master_key = true
 
+  config.static_cache_control = "public, max-age=31536000"
+
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.headers = {
+    'Cache-Control' => 'public, max-age=31536000',
+    'Expires' => 1.year.from_now.to_formatted_s(:rfc822)
+  }
 
   # Compress CSS using a preprocessor.
   config.assets.css_compressor = :sass
