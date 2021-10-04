0f31a4f49d8dace9deadade0547d9ac92fadfc08
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2021-10-04 13:37:19 +0200

    Update CORS settings

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    resource '/assets/*', headers: :any, methods: :any
+    resource '/assets/*', headers: :any, methods: %i[get options], credentials: false
+    resource '/packs/*', headers: :any, methods: %i[get options], credentials: false
   end
 end
