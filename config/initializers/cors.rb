7f248a95bdf266de00a128bda194de59eb7c13d2
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2021-10-04 13:02:35 +0200

    Allow any CORS origins for /assets/*

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  if ENV['ASSET_HOST']
-    allow do
-      origins "#{ENV['ASSET_HOST']}:443"
-      resource '*', headers: :any, methods: %i[get options head]
-    end
+  allow do
+    origins '*'
+    resource '/assets/*', headers: :any, methods: %i[get options head]
   end
 end
