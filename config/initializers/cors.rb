2bc295ec82a2a44a30b6843a0963a29747111ffb
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2021-10-04 13:11:20 +0200

    Disable Asset compilation on production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
-    resource '/assets/*', headers: :any, methods: %i[get options head]
+    resource '/assets/*', headers: :any, methods: :any
   end
 end
