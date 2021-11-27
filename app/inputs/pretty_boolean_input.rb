9775b5a5bcfe4117446489135217d5b87155d1ac
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2021-11-27 10:32:49 +0100

    Remove Pretty inputs classes
    They are unused now.

--- app/inputs/pretty_boolean_input.rb
+++
@@ -1,6 +0,0 @@
-class PrettyBooleanInput < Formtastic::Inputs::BooleanInput
-  def label_text
-    template.content_tag(:span, nil, class: 'checkmark') <<
-      template.content_tag(:span, super, class: 'label')
-  end
-end
