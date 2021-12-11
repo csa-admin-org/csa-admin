f84778a0fcad924e9d371b20468211e1121635b0
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2021-12-11 13:29:00 +0100

    Fix migration

--- db/migrate/20211211105616_rename_depot_form_names_to_public_names.rb
+++ db/migrate/20211211105616_rename_depot_form_names_to_public_names.rb
@@ -1,5 +1,5 @@
 class RenameDepotFormNamesToPublicNames < ActiveRecord::Migration[6.1]
   def change
-    rename_column :depots, :public_names, :public_names
+    rename_column :depots, :form_names, :public_names
   end
 end
