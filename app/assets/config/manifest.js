9e4819f05ab19718d1d56f5d3e720d80f1526771
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2021-12-28 10:55:57 +0100

    Fix CI

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link tailwind.css
 //= link_tree ../../../vendor/javascript .js
 //= link_tree ../../javascript .js
 //= link_tree ../builds
