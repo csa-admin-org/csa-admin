42b287e8fc038c36211a627a126a4a363e63bbec
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2021-12-30 14:37:12 +0100

    Add Hanbook page 📖
    This is used to write guide about some ACP Admin features, more content should be added in future.

---
+++ app/admin/handbook.rb
@@ -0,0 +1,63 @@
+ActiveAdmin.register_page 'Handbook' do
+  menu false
+
+  content title: I18n.t('layouts.footer.handbook') do
+    if params[:id]
+      columns do
+        handbook = Handbook.new(params[:id],binding)
+        handbook.body
+      end
+    else
+      columns do
+        h1 t('.handbook_intro_title')
+        para t('.handbook_intro_html'), class: 'notice'
+      end
+    end
+  end
+
+  sidebar I18n.t('active_admin.page.index.pages') do
+    ul class: 'handbook-toc' do
+      li do
+        if params[:id]
+          a href: handbook_path do
+            span t('.handbook_intro_title')
+          end
+        else
+          span t('.handbook_intro_title'), class: 'active'
+        end
+      end
+      Handbook.all(binding).each do |handbook|
+        li class: (handbook.name == params[:id] ? 'active' : '') do
+          if handbook.name == params[:id]
+            span class: 'active' do
+              span inline_svg_tag('admin/chevron-down.svg', size: '12')
+              span handbook.title
+            end
+            ul do
+              handbook.subtitles.each do |subtitle, id|
+                li do
+                  a href: handbook_page_path(handbook.name, anchor: id) do
+                    span subtitle
+                  end
+                end
+              end
+            end
+          else
+            a href: handbook_page_path(handbook.name) do
+              span inline_svg_tag('admin/chevron-right.svg', size: '12')
+              span handbook.title
+            end
+          end
+        end
+      end
+    end
+  end
+
+  sidebar I18n.t('active_admin.page.index.help'), if: -> { params[:id] } do
+    div class: 'content' do
+      para t('.handbook_questions_html')
+    end
+  end
+
+  config.breadcrumb = false
+end
---
+++ app/models/handbook.rb
@@ -0,0 +1,47 @@
+class Handbook
+  include Comparable
+
+  DIR_PATH = 'app/views/handbook'
+  attr_reader :name
+
+  def self.all(context)
+    path = Rails.root.join(DIR_PATH, '*.md.erb')
+    Dir.glob(path).map { |path|
+      name = File.basename(path, '.md.erb')
+      new(name, context)
+    }.sort
+  end
+
+  def initialize(name, context)
+    @name = name
+    @context = context
+  end
+
+  def filepath
+    Rails.root.join(DIR_PATH, "#{name}.md.erb")
+  end
+
+  def body
+    @body ||= begin
+      body = File.read(filepath)
+      result = ERB.new(body).result(@context)
+      Kramdown::Document.new(result).to_html.html_safe
+    end
+  end
+
+  def doc
+    @doc ||= Nokogiri::HTML::DocumentFragment.parse(body)
+  end
+
+  def title
+    @title ||= doc.css('h1').map(&:text).first
+  end
+
+  def subtitles
+    @subtitles ||= doc.css('h2').map { |h2| [h2.text, h2[:id]] }
+  end
+
+  def <=>(other)
+    title <=> other.title
+  end
+end
