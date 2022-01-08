3132d68449a543807076b6ead00ccf966dd6bc69
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2022-01-08 17:26:30 +0100

    Fix delivery related spec

--- spec/models/basket_spec.rb
+++ spec/models/basket_spec.rb
@@ -71,6 +71,7 @@ describe Basket do
     create(:basket_complement, id: 1, price: 3.2)
     create(:basket_complement, id: 2, price: 4.5)
 
+    create(:delivery)
     membership_1 = create(:membership, subscribed_basket_complement_ids: [1, 2])
     membership_2 = create(:membership, subscribed_basket_complement_ids: [2])
     delivery = create(:delivery, basket_complement_ids: [1, 2])
