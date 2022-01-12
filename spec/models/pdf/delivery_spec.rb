c653bb8bebac6117ede1da15f5a4a177ee84dcdd
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2022-01-12 17:16:25 +0100

    Inclusive fix

--- spec/models/pdf/delivery_spec.rb
+++ spec/models/pdf/delivery_spec.rb
@@ -77,7 +77,7 @@ describe PDF::Delivery do
         .and contain_sequence('Totaux (dépôt)', '2', '1', '3', '1', 'Signature')
         .and contain_sequence('Alain Reymond', '1', '1', '1')
         .and contain_sequence('John Doe', '2', '2')
-        .and contain_sequence('Missing Joe', '–', '–', '–', '–', 'ABSENT(E)')
+        .and contain_sequence('Missing Joe', '–', '–', '–', '–', 'ABSENT·E')
         .and contain_sequence("Si vous avez des remarques ou problèmes, veuillez contacter Julien (079 705 89 01) jusqu'au vendredi midi.")
       expect(pdf_strings).not_to include 'Jame Dane'
       expect(pdf_strings).not_to include 'Moyen PUBLIC'
