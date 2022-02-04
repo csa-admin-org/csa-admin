a1630f1c8d2c04ba130400badb95cab8d4c78f64
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2022-02-04 08:46:31 +0100

    DeliveriesCycleBasketsUpdaterJob, only handle current and future memberships

--- app/jobs/deliveries_cycle_baskets_updater_job.rb
+++ app/jobs/deliveries_cycle_baskets_updater_job.rb
@@ -2,6 +2,6 @@ class DeliveriesCycleBasketsUpdaterJob < ApplicationJob
   queue_as :default
 
   def perform(deliveries_cycle)
-    MembershipBasketsUpdater.perform_all!(deliveries_cycle.memberships)
+    MembershipBasketsUpdater.perform_all!(deliveries_cycle.memberships.current_or_future)
   end
 end
