f512d0cfdf48962c3dddb7f822c6e2dea5cb676a
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2022-03-04 10:33:06 +0100

    TranslatedAttributes, fallback to default locale
    This is useful when an admin went to use a locale not supported by the ACP.

--- app/models/concerns/translated_attributes.rb
+++ app/models/concerns/translated_attributes.rb
@@ -5,7 +5,10 @@ module TranslatedAttributes
     def translated_attributes(*attrs)
       attrs.each do |attr|
         column = attr.to_s.pluralize
-        define_method(attr) { |locale = I18n.locale| self[column][locale.to_s].presence }
+        define_method(attr) { |locale = I18n.locale|
+          self[column][locale.to_s].presence ||
+            self[column][Current.acp.default_locale.to_s].presence
+        }
         define_method("#{attr}=") { |str| self[column][I18n.locale.to_s] = str }
         ACP::LANGUAGES.each do |locale|
           define_method("#{attr}_#{locale}") do
