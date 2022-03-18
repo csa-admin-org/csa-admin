db9d8c5f27608c6115877a8491ce267b29df1f81
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2022-03-18 14:00:06 +0100

    Only enable sentry on production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :prepare_exception_notifier
   before_action :set_locale
 
   helper_method :current_admin, :current_session
@@ -74,12 +73,6 @@ class ApplicationController < ActionController::Base
     Sentry.set_user(
       id: "admin_#{current_admin.id}",
       session_id: current_session.id)
-  end
-
-  def prepare_exception_notifier
-    request.env['exception_notifier.exception_data'] = {
-      current_acp: Apartment::Tenant.current
-    }
   end
 
   def create_session_from_devise_remember_token!
