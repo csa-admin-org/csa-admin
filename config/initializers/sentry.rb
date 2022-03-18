7398de60f896c44dceac05aa2a43450d78501769
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2022-03-18 14:00:06 +0100

    Only enable sentry on production

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,4 +1,5 @@
 Sentry.init do |config|
   config.breadcrumbs_logger = [:active_support_logger]
   config.traces_sample_rate = ENV['SENTRY_TRACES_SAMPLE_RATE']&.to_f || 0.1
+  config.enabled_environments = %w[production]
 end
