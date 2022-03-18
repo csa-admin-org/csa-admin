3d35857bac6564494bd5f3e76fbeb54fbec68046
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2022-03-18 15:19:02 +0100

    Improve Tenant active storage patch

--- lib/tenant/activestorage_patch.rb
+++ lib/tenant/activestorage_patch.rb
@@ -1,10 +1,12 @@
 # Fix rails_blob_path not using the right schema when linking to a blob
+# https://github.com/rails-on-services/apartment/issues/196
 module ActiveRecordSignedIDACP
   def find_signed!(signed_id, purpose: nil)
     if id = signed_id_verifier.verify(signed_id, purpose: combine_signed_id_purposes(purpose))
-      ACP.perform(Current.acp.tenant_name) do
-        find(id)
-      end
+      # Ensure the connection schema_search_path is well set
+      raise 'Tenant outside!' if Tenant.outside?
+      Tenant.connect(Tenant.current)
+      find(id)
     end
   end
 end
