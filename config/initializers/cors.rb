301bfc3606705dd467572325ad42d7f02ba8e640
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2022-03-20 11:43:18 +0100

    Drop web packs in CORS config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,6 +9,5 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
     resource '/assets/*', headers: :any, methods: %i[get options], credentials: false
-    resource '/packs/*', headers: :any, methods: %i[get options], credentials: false
   end
 end
