a9939ce297d602db5763f25306928e8f84c7f274
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2022-04-01 15:47:01 +0200

    Only enable Sentry.js on production

--- app/javascript/components/sentry.js
+++ app/javascript/components/sentry.js
@@ -2,5 +2,6 @@ import * as Sentry from "@sentry/browser";
 
 Sentry.init({
   dsn: "https://98d2ed9e0cb845cabc1fff00323e06ca@o555399.ingest.sentry.io/5685152",
-  tracesSampleRate: 0.05,
+  environment: "production",
+  tracesSampleRate: 0.05
 });
