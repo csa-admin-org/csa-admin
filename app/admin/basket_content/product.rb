1fea60d7b871d31e29e0858800268b635eed789a
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2022-05-20 14:50:01 +0200

    BasketContent::Product, improve listing and CSV export
    It now includes the lastest basket content usage for more clarity.

--- app/admin/basket_content/product.rb
+++ app/admin/basket_content/product.rb
@@ -14,13 +14,38 @@ class BasketContent
       links
     end
 
-    includes :basket_contents
+    includes :basket_contents, :latest_basket_content
     index do
       column :name
-      column :url, ->(bc) { link_to(bc.url_domain, bc.url) if bc.url? }
+      column :url, ->(p) { link_to(p.url_domain, p.url) if p.url? }
+      column(:latest_use) { |p|
+        if p.latest_basket_content
+          display_with_unit_price(p.latest_basket_content.unit_price, p.latest_basket_content.unit) {
+            link_to(
+              l(p.latest_basket_content.delivery.date),
+              basket_contents_path(q: { delivery_id_eq: p.latest_basket_content.delivery_id }))
+          }
+        end
+      }
       if authorized?(:update, Product)
         actions class: 'col-actions-2'
       end
+    end
+
+    csv do
+      column(:id)
+      column(:name)
+      column(:url)
+      column(:latest_delivery) { |p|
+        p.latest_basket_content&.delivery&.date
+      }
+      column(:latest_unit) { |p|
+        if p.latest_basket_content
+          I18n.t("units.#{p.latest_basket_content.unit}")
+        end
+      }
+      column(:latest_quantity) { |p| p.latest_basket_content&.quantity }
+      column(:latest_unit_price) { |p| p.latest_basket_content&.unit_price }
     end
 
     show do
