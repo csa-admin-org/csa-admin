a63e816d8c22a7131d2c9f5eeea26853b777ca52
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2022-05-20 10:11:12 +0200

    JS, improve basket delivery/depot select logic
    This patch ensures that when changing a basket delivery the existing
    depot stays selected if it's still part of the delivery available depots.

--- app/assets/javascripts/components/form.js
+++ app/assets/javascripts/components/form.js
@@ -60,17 +60,20 @@ $(document).on("turbolinks:load", function() {
 
   $(".js-update_basket_depot_options").on("change", function() {
     var selectedDeliveryID = this.value;
-    Array.from(document.querySelector("#basket_depot_id").options).forEach(
-      (option) => {
-        var deliveryIds = option.getAttribute("data-delivery-ids").split(",");
-        if (deliveryIds.some((id) => id === selectedDeliveryID)) {
-          option.disabled = false;
-          option.selected = false;
-        } else {
-          option.disabled = true;
-          option.selected = false;
-        }
+    var depotSelect = document.querySelector("#basket_depot_id");
+    var selectedDepotID = depotSelect.value;
+    Array.from(depotSelect.options).forEach(option => {
+      var deliveryIds = option.getAttribute("data-delivery-ids").split(",");
+      if (deliveryIds.some((id) => id === selectedDeliveryID)) {
+        option.disabled = false;
+        option.selected = false;
+      } else {
+        option.disabled = true;
+        option.selected = false;
       }
-    );
+      if (option.value === selectedDepotID && !option.disabled) {
+        option.selected = true;
+      }
+    });
   });
 });
