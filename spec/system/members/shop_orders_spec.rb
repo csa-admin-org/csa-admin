aab7bf99ac69189be14b3390d794ffd9327b4fc7
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2022-05-27 10:42:39 +0200

    Shop, handle unavailable product variant in cart
    It could happen that a product or its variants is made unavailable
    while it was already in the cart. In this case we need the member must
    remove it before it can confirm the order.
    
    Previously the confirm button was disabled but it wasn't clear why it
    was the case, now an "unavailable" label is shown next to the order item
    and the "+" button is disabled as well.

--- spec/system/members/shop_orders_spec.rb
+++ spec/system/members/shop_orders_spec.rb
@@ -95,6 +95,40 @@ describe 'Shop::Order' do
     end
   end
 
+  specify 'unavailable product in cart order' do
+    travel_to '2021-11-08' do
+      delivery = create(:delivery, shop_open: true, date: '2021-11-10')
+      create(:membership, member: member, started_on: '2021-11-01', ended_on: '2021-11-30')
+      order = create(:shop_order, :cart,
+        delivery: Delivery.last,
+        member: member,
+        items_attributes: {
+          '0' => {
+            product_id: product1.id,
+            product_variant_id: product1.variants.first.id,
+            quantity: 1
+          },
+          '1' => {
+            product_id: product2.id,
+            product_variant_id: product2.variants.first.id,
+            quantity: 1
+          }
+        })
+
+      product1.update!(available: false)
+
+      visit "/shop/orders/#{order.id}"
+      expect(current_path).to eq "/shop/orders/#{order.id}"
+
+      within 'label[for="shop_order_items_attributes_0_quantity"]' do
+        expect(page).to have_content('indisponible')
+      end
+
+      button = find('button[aria-label="confirm_order"]')
+      expect(button).to be_disabled
+    end
+  end
+
   specify 'cart can be finalize depending date' do
     order = nil
     travel_to '2021-11-08 11:59 +01' do
