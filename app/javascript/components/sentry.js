6d3304fc40097c92a2dd70b3476d84c7895cded7
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2022-06-10 11:52:29 +0200

    Drop debug from Sentry JS config

--- app/javascript/components/sentry.js
+++ app/javascript/components/sentry.js
@@ -3,6 +3,5 @@ import * as Sentry from "@sentry/browser";
 Sentry.init({
   dsn: "https://98d2ed9e0cb845cabc1fff00323e06ca@o555399.ingest.sentry.io/5685152",
   environment: "production",
-  tracesSampleRate: 0.05,
-  debug: true
+  tracesSampleRate: 0.05
 });
