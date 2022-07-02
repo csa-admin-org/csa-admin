d442b04b62b7ef9462127f221a4077ee10f2bc56
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2022-07-02 10:22:06 +0200

    Scheduled jobs, stop retrying for more than a day
    The `retry_on` option isn't enough as Sidekiq retries with exponential
    backoff is still taking over after the number of attempts specified.
    
    This patch makes use of `sidekiq_options` retries limit instead.
    
    https://github.com/mperham/sidekiq/wiki/Active-Job#customizing-error-handling

--- app/jobs/scheduled/base_job.rb
+++ app/jobs/scheduled/base_job.rb
@@ -1,5 +1,6 @@
 module Scheduled
   class BaseJob < ApplicationJob
+    sidekiq_options retry: 5
     queue_as :low
   end
 end
--- app/jobs/scheduled/notifier_job.rb
+++ app/jobs/scheduled/notifier_job.rb
@@ -1,7 +1,5 @@
 module Scheduled
   class NotifierJob < BaseJob
-    retry_on StandardError, attempts: 10
-
     def perform
       Notifier.send_all
     end
