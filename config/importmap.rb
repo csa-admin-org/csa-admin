3035598dc9783b2fb639d936ac37f0ee0a0b643f
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2022-09-21 20:26:10 +0200

    Upgrade dependencies

--- config/importmap.rb
+++ config/importmap.rb
@@ -33,7 +33,7 @@ pin "ace-builds/src-noconflict/theme-dreamweaver", to: "ace-builds--src-noconfli
 pin "tom-select" # @2.1.0
 
 # Sentry
-pin "@sentry/browser", to: "https://ga.jspm.io/npm:@sentry/browser@7.12.0/esm/index.js"
-pin "@sentry/core", to: "https://ga.jspm.io/npm:@sentry/core@7.12.0/esm/index.js"
-pin "@sentry/hub", to: "https://ga.jspm.io/npm:@sentry/hub@7.12.0/esm/index.js"
-pin "@sentry/utils", to: "https://ga.jspm.io/npm:@sentry/utils@7.12.0/esm/index.js"
+pin "@sentry/browser", to: "https://ga.jspm.io/npm:@sentry/browser@7.13.0/esm/index.js"
+pin "@sentry/core", to: "https://ga.jspm.io/npm:@sentry/core@7.13.0/esm/index.js"
+pin "@sentry/hub", to: "https://ga.jspm.io/npm:@sentry/hub@7.13.0/esm/index.js"
+pin "@sentry/utils", to: "https://ga.jspm.io/npm:@sentry/utils@7.13.0/esm/index.js"
