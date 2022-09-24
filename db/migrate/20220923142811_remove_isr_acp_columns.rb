fbe36360ffb664fab8cbf9324467f371e95f263e
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2022-09-24 09:58:57 +0200

    Fix migration

--- db/migrate/20220923142811_remove_isr_acp_columns.rb
+++ db/migrate/20220923142811_remove_isr_acp_columns.rb
@@ -1,4 +1,4 @@
-class RemoveISRACPColumns < ActiveRecord::Migration[7.0]
+class RemoveIsrACPColumns < ActiveRecord::Migration[7.0]
   def change
     remove_column :acps, :ccp
     remove_column :acps, :isr_identity
