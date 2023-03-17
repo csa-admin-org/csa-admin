csa-admin-org/csa-admin

commit ea8a80e0f21b94bae0b03113d0bd02f6c09fcba3

Shop order form improvement
This patch ensures that the product variant selected is always enabled.

Previously it was possible that a disabled variant got selected by default.

app/javascript/controllers/admin/form_select_options_filter_controller.js
@@ -15,21 +15,15 @@ export default class extends Controller {
       Array.from(select.options).forEach((option) => {
         const values = option.getAttribute(this.attributeValue)?.split(',')
         if (values && values.some((v) => v === event.currentTarget.value.toString())) {
-          option.disabled = option.getAttribute("data-disabled") == "true"
-          option.hidden = option.getAttribute("data-disabled") == "true"
-          option.selected = option.getAttribute("data-disabled") == "true"
+          option.disabled = option.getAttribute("data-disabled") === "true"
+          option.hidden = option.getAttribute("data-disabled") === "true"
         } else {
           option.disabled = true
           option.hidden = true
-          option.selected = false
         }
-        if (option.value === selectedValue && !option.disabled) {
-          option.selected = true;
-        }
+        option.selected = false
       })
-      if (!select.value) {
-        Array.from(select.options).find((o) => !o.disabled).selected = true
-      }
+      Array.from(select.options).find((o) => !o.disabled).selected = true
     })
   }
 }

commit 69c3e89ae86b3cbff2e600e9eebc22b0812ee5c9

JS, keep the current selected value selected
This has been broken by
https://github.com/acp-admin/acp-admin/commit/ea8a80e0f21b94bae0b03113d0bd02f6c09fcba3

app/javascript/controllers/admin/form_select_options_filter_controller.js
@@ -22,8 +22,14 @@ export default class extends Controller {
           option.hidden = true
         }
         option.selected = false
+        // Keep the current selected value selected
+        if (option.value === selectedValue && !option.disabled) {
+          option.selected = true
+        }
       })
-      Array.from(select.options).find((o) => !o.disabled).selected = true
+      if (!select.value) {
+        Array.from(select.options).find((o) => !o.disabled).selected = true
+      }
     })
   }
 }