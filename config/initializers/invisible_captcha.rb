57500b5ca506ee0a9fe3d013054c052829255025
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2023-06-24 10:46:46 +0200

    Fine tune member form

--- config/initializers/invisible_captcha.rb
+++ config/initializers/invisible_captcha.rb
@@ -1,8 +1,8 @@
 InvisibleCaptcha.setup do |config|
   # config.honeypots           << ['more', 'fake', 'attribute', 'names']
   # config.visual_honeypots    = false
-  # config.timestamp_threshold = 4
-  # config.timestamp_enabled   = true
+  config.timestamp_threshold = 2
+  config.timestamp_enabled   = true
   # config.injectable_styles   = false
 
   # Leave these unset if you want to use I18n (see below)
