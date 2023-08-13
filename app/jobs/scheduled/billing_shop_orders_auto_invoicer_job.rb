d2eec3378b6ec0e2f52ff0e8d99a779cfe9df4a7
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2023-08-13 10:30:14 +0200

    Fix BillingShopOrdersAutoInvoicerJob bug
    https://acp-admin.sentry.io/issues/4391327803

--- app/jobs/scheduled/billing_shop_orders_auto_invoicer_job.rb
+++ app/jobs/scheduled/billing_shop_orders_auto_invoicer_job.rb
@@ -5,7 +5,7 @@ module Scheduled
       return unless Current.acp.shop_order_automatic_invoicing_delay_in_days
 
       Shop::Order.pending.find_each do |order|
-        Billing::ShopOrderInvoicerJob.perform_later(order)
+        Billing::ShopOrderAutoInvoicerJob.perform_later(order)
       end
     end
   end
