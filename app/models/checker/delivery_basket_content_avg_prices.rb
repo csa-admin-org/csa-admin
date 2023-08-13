2651f91eede7b96124bad0f70e8865cb28e22442
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2023-08-13 10:35:45 +0200

    Improve DeliveryBasketContentAvgPrices check
    Only notify meaningful diff.

--- app/models/checker/delivery_basket_content_avg_prices.rb
+++ app/models/checker/delivery_basket_content_avg_prices.rb
@@ -1,5 +1,7 @@
 module Checker
   class DeliveryBasketContentAvgPrices
+    MAX_DIFF = 0.001
+
     def self.check_all!
       Delivery.where(date: 1.month.ago..1.month.from_now).find_each do |m|
         new(m).check!
@@ -11,9 +13,11 @@ module Checker
     end
 
     def check!
-      previous = @delivery.basket_content_avg_prices.dup
+      before = @delivery.basket_content_avg_prices.dup
       @delivery.update_basket_content_avg_prices!
-      if previous != @delivery.basket_content_avg_prices
+      after = @delivery.basket_content_avg_prices
+
+      if (before - after).abs >= MAX_DIFF
         Sentry.capture_message('Delivery basket content avg prices mismatch', extra: {
           delivery_id: @delivery.id,
           previous: previous,
