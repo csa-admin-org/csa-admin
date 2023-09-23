89c7c858af9acd4ac7c42042f951228b757ff7a0
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2023-09-23 19:59:39 +0200

    Add description method to MembersBasketComplement

--- app/models/members_basket_complement.rb
+++ app/models/members_basket_complement.rb
@@ -1,7 +1,13 @@
 class MembersBasketComplement < ApplicationRecord
+  include HasDescription
+
   belongs_to :member, touch: true
   belongs_to :basket_complement
 
   validates :basket_complement_id, uniqueness: { scope: :member_id }
   validates :quantity, numericality: { greater_than_or_equal_to: 1 }, presence: true
+
+  def description(public_name: false)
+    describe(basket_complement, quantity, public_name: public_name)
+  end
 end
