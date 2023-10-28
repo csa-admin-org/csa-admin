80c7add6344b288786accb51a09251b724225daa
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2023-10-28 10:11:36 +0200

    Fix migration

--- db/migrate/20231027134358_rename_invoices_object_to_entity.rb
+++ db/migrate/20231027134358_rename_invoices_object_to_entity.rb
@@ -1,6 +1,6 @@
 class RenameInvoicesObjectToEntity < ActiveRecord::Migration[7.0]
   def change
     rename_column :invoices, :object_id, :entity_id
-    rename_column :invoices, :entity_type, :entity_type
+    rename_column :invoices, :object_type, :entity_type
   end
 end
