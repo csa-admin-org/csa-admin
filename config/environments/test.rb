968170dd9615b588efc787add06a7ef387389f17
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2023-11-25 10:52:38 +0100

    Disable logging in test environment
    This should speed up the tests a bit.
    https://jtway.co/speed-up-your-rails-test-suite-by-6-in-1-line-13fedb869ec4

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -61,4 +61,9 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  unless ENV['RAILS_ENABLE_TEST_LOG']
+    config.logger = Logger.new(nil)
+    config.log_level = :fatal
+  end
 end
