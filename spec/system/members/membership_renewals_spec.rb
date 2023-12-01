006d6ffbfa75d8ee08c4b169ca8f7903dff1620b
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2023-12-01 15:15:04 +0100

    Membership renewal fix, hide basket price extra for salary basket
    This patch hides the basket price extra list in the membership renewal
    form for member with the salary basket mode on. As those members aren't
    paying their baskets, it's confusing to show them the price extra list
    as it will not be used anyway.
    
    Fix https://acp-admin.sentry.io/issues/4682013884

--- spec/system/members/membership_renewals_spec.rb
+++ spec/system/members/membership_renewals_spec.rb
@@ -124,7 +124,7 @@ describe 'Memberships Renewal' do
 
   specify 'renew membership (with basket_price_extra)', freeze: '2020-09-30' do
     Current.acp.update!(
-      basket_price_extra_title: 'Cotistation solidaire',
+      basket_price_extra_public_title: 'Cotistation solidaire',
       basket_price_extras: '0, 1, 2, 4, 8',
       basket_price_extra_label: "+ {{ extra | ceil }}.-/panier")
     membership = create(:membership,
@@ -144,6 +144,8 @@ describe 'Memberships Renewal' do
     click_on 'Suivant'
 
     choose "Grand PUBLIC"
+
+    expect(page).to have_content 'Cotistation solidaire'
     choose "+ 8.-/panier"
 
     fill_in 'Remarque(s)', with: "Plus d'épinards!"
@@ -176,6 +178,64 @@ describe 'Memberships Renewal' do
       ended_on: Date.parse('2021-12-31'),
       basket_size: big_basket,
       basket_price_extra: 8)
+  end
+
+  specify 'renew membership (with basket_price_extra but salary basket)', freeze: '2020-09-30' do
+    Current.acp.update!(
+      basket_price_extra_public_title: 'Cotistation solidaire',
+      basket_price_extras: '0, 1, 2, 4, 8',
+      basket_price_extra_label: "+ {{ extra | ceil }}.-/panier")
+    member.update!(salary_basket: true)
+    membership = create(:membership,
+      member: member,
+      basket_size: basket_size,
+      depot: depot)
+    create_deliveries(1, Current.acp.fiscal_year_for(2021))
+    big_basket = create(:basket_size, name: 'Grand')
+    membership.open_renewal!
+
+    login(member)
+
+    expect(menu_nav).to include("Abonnement\n⤷ Renouvellement ?")
+    click_on 'Abonnement'
+
+    choose 'Renouveler mon abonnement'
+    click_on 'Suivant'
+
+    choose "Grand PUBLIC"
+
+    expect(page).not_to have_content 'Cotistation solidaire'
+
+    fill_in 'Remarque(s)', with: "Plus d'épinards!"
+
+    click_on 'Confirmer'
+
+    expect(page).to have_selector('.flash',
+      text: 'Votre abonnement a été renouvelé. Merci!')
+
+    expect(menu_nav).to include("Abonnement\n⤷ En cours")
+    within 'ul#2021' do
+      expect(page).to have_content '1 janvier 2021 – 31 décembre 2021'
+      expect(page).to have_content 'Grand PUBLIC'
+      expect(page).to have_content 'Joli Lieu'
+      expect(page).to have_content '1 Livraison'
+      expect(page).to have_content '½ Journées: Aucun engagement'
+      expect(page).to have_content "Paniers salaire"
+    end
+    expect(membership.reload).to have_attributes(
+      renew: true,
+      renewal_annual_fee: nil,
+      renewal_opened_at: Time.current,
+      renewed_at: Time.current,
+      renewal_note: "Plus d'épinards!",
+      basket_price_extra: 0)
+    expect(membership).to be_renewed
+    expect(membership.renewed_membership).to have_attributes(
+      renew: true,
+      started_on: Date.parse('2021-01-01'),
+      ended_on: Date.parse('2021-12-31'),
+      basket_size: big_basket,
+      basket_price_extra: 0)
   end
 
   specify 'cancel membership', freeze: '2020-09-30' do
