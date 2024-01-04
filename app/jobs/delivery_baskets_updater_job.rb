1d6d3c3864aea7b87bc5f0c94dfc4869a389e76d
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2024-01-04 09:54:37 +0100

    DeliveryBasketsUpdaterJob, fix job uniqueness handling
    As activejob-uniqueness does not support the Sidekiq tags used to store
    the tenant, we need to explicitly pass the tenant to the job.

--- app/jobs/delivery_baskets_updater_job.rb
+++ app/jobs/delivery_baskets_updater_job.rb
@@ -2,14 +2,10 @@ class DeliveryBasketsUpdaterJob < ApplicationJob
   queue_as :default
   unique :until_executing, on_conflict: :log
 
-  def perform(year)
+  # _tenant is a hack to make this job unique per tenant, as activejob-uniqueness
+  # doesn't support the Sidekiq tags used to switch tenant.
+  def perform(year, _tenant)
     memberships = Membership.during_year(year)
     MembershipBasketsUpdater.perform_all!(memberships)
-  end
-
-  private
-
-  def lock_key_arguments
-    [ Current.acp.tenant_name ] + arguments
   end
 end
