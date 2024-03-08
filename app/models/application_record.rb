090bb114f2a821338d33b84741780ecffafb9668
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2024-03-08 15:48:27 +0100

    Invoice, ensure continuous invoice numbering
    This patch makes sures than only the latest invoice can be destroyed and that
    the invoice primary key sequence is reset when doing so to ensure continuous
    invoice numbering as the id is used as invoice number.

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -11,4 +11,8 @@ class ApplicationRecord < ActiveRecord::Base
   def self.ransackable_associations(auth_object = nil)
     authorizable_ransackable_associations
   end
+
+  def self.reset_pk_sequence!
+    self.connection.reset_pk_sequence!(self.table_name)
+  end
 end
