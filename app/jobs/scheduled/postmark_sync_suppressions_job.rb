e8049256359f8535f5aea02656549ef0be58f58c
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2024-05-06 20:51:26 +0200

    Postmark fine-tuning

--- app/jobs/scheduled/postmark_sync_suppressions_job.rb
+++ app/jobs/scheduled/postmark_sync_suppressions_job.rb
@@ -1,6 +1,6 @@
 module Scheduled
   class PostmarkSyncSuppressionsJob < BaseJob
-    sidekiq_options retry: false
+    sidekiq_options retry: 3
 
     def perform
       EmailSuppression.sync_postmark!(fromdate: 1.week.ago)
