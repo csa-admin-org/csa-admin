354f3022ccab1ebaab5a44c34e4ffb9737b34606
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2024-05-10 11:41:16 +0200

    Drop acp.delivery_pdf_show_phones

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_10_085438) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_10_093951) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
   enable_extension "plpgsql"
@@ -58,7 +58,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_10_085438) do
     t.string "activity_phone"
     t.decimal "activity_price", precision: 8, scale: 2, default: "0.0", null: false
     t.boolean "absences_billed", default: true, null: false
-    t.boolean "delivery_pdf_show_phones", default: false, null: false
     t.integer "recurring_billing_wday"
     t.string "email_notifications", default: [], null: false, array: true
     t.string "feature_flags", default: [], null: false, array: true
