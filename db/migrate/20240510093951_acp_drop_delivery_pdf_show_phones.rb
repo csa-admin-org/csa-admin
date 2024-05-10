4630605fbd45f1bf583c906ebe17a4bb6a262026
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2024-05-10 11:41:16 +0200

    Drop acp.delivery_pdf_show_phones

---
+++ db/migrate/20240510093951_acp_drop_delivery_pdf_show_phones.rb
@@ -0,0 +1,5 @@
+class ACPDropDeliveryPDFShowPhones < ActiveRecord::Migration[7.1]
+  def change
+    remove_column :acps, :delivery_pdf_show_phones, :boolean, default: false, null: false
+  end
+end
