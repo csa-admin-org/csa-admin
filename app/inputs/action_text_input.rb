74b4cb992499d34cf0b2bfeeeb491e49f274a9d3
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2024-06-22 11:32:43 +0200

    Fix frozen_string_literal
    FrozenError: can't modify frozen String: "long-text" (FrozenError)
      from app/inputs/action_text_input.rb:6:in `block in to_html'
      from actionview (7.1.3.4) lib/action_view/helpers/capture_helper.rb:50:in `block in capture'
      from actionview (7.1.3.4) lib/action_view/buffers.rb:75:in `capture'
      from actionview (7.1.3.4) lib/action_view/helpers/capture_helper.rb:50:in `capture'
      from formtastic (5.0.0) lib/formtastic/inputs/base/wrapping.rb:12:in `input_wrapping'

--- app/inputs/action_text_input.rb
+++ app/inputs/action_text_input.rb
@@ -3,7 +3,7 @@
 class ActionTextInput < Formtastic::Inputs::StringInput
   def to_html
     input_wrapping do
-      input_html_options[:class] << " trix-content"
+      input_html_options[:class] += " trix-content"
       editor_tag = builder.rich_text_area(method, input_html_options)
       label_html + editor_tag
     end
