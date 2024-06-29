d38667c2aa7e89611871f9520115cd653ac9ebdd
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2024-06-29 15:46:12 +0200

    Enable Sentry Metrics

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,8 @@
-# frozen_string_literal: true
+  # frozen_string_literal: true
 
 Sentry.init do |config|
   config.breadcrumbs_logger = [ :active_support_logger ]
+  config.metrics.enabled = true
   config.traces_sample_rate = ENV["SENTRY_TRACES_SAMPLE_RATE"]&.to_f || 0.1
   config.enabled_environments = %w[production]
 end
