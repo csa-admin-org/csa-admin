68f68b448976b23893f56830b4e16f7ea51131d7
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2024-07-09 08:56:12 +0200

    Newsletter, limit file size

--- app/javascript/admin/trix.js
+++ app/javascript/admin/trix.js
@@ -2,8 +2,13 @@ import "trix"
 import "@rails/actiontext"
 
 const allowedTypes = ["image/png", "image/jpg", "image/jpeg", "image/gif"]
+const maxImageSize = 512000
 
 document.addEventListener("trix-file-accept", e => {
+  if (e.file.size > maxImageSize) {
+    e.preventDefault()
+    alert("Only images smaller than 512KB are allowed!")
+  }
   if (!allowedTypes.includes(e.file.type)) {
     e.preventDefault()
     alert("Only images are allowed!")
