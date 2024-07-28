1e0593dc4cff13db7b41299028a3c9998761c2cd
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2024-07-28 10:07:14 +0200

    Fix member dark mode switch
    https://acp-admin.sentry.io/issues/5628056267

--- app/javascript/members.js
+++ app/javascript/members.js
@@ -8,10 +8,3 @@ window.document.addEventListener("turbo:submit-end", (_event) => {
     window.initFlowbite()
   }, 10)
 })
-
-// https://flowbite.com/docs/customize/dark-mode/#dark-mode-switcher
-if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-  document.documentElement.classList.add('dark')
-} else {
-  document.documentElement.classList.remove('dark')
-}
