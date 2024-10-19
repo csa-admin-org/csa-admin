43066296da9279727d24058f4c0d42751f304dda
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2024-10-19 18:52:17 +0200

    Setup litestream-ruby
    This patch adds the litestream-ruby gem and configures it.
    
    This allows to easily stream backups of the SQLite database to Backblaze B2.
    So far only the `production_queue.sqlite3` is backed up, but once all tenants
    are using SQLite, we can add the other databases as well.
    
    - https://litestream.io
    - https://github.com/fractaledmind/litestream-ruby

--- app/controllers/mission_control_controller.rb
+++ app/controllers/mission_control_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class MissonControlController < ActionController::Base
+class MissionControlController < ActionController::Base
   if password = ENV["MISSION_CONTROL_PASSWORD"]
     http_basic_authenticate_with name: "mc", password: password
   end
--- config/application.rb
+++ config/application.rb
@@ -45,7 +45,7 @@ module CSAAdmin
     config.action_mailer.preview_paths = [ "#{Rails.root}/app/mailer_previews" ]
 
 
-    config.mission_control.jobs.base_controller_class = "MissonControlController"
+    config.mission_control.jobs.base_controller_class = "MissionControlController"
 
     # https://github.com/heartcombo/responders?tab=readme-ov-file#configuring-error-and-redirect-statuses
     config.responders.error_status = :unprocessable_entity
