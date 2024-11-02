4c01c36f43292eb8560fc6dbc79a0c7e797adbc1
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2024-11-02 21:33:20 +0100

    Drop RUBY_DISABLE_YJIT env var
    No more needed, it's enable and running fine.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,8 +82,4 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-
-  if ENV["RUBY_DISABLE_YJIT"] == "true"
-    config.yjit = false
-  end
 end
