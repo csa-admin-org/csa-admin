35caf5b477178849d53ec8c335cad6b2d1cb073d
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2024-11-16 13:46:26 +0100

    ActiveJob TenantSwitcher fix
    This patch makes sure that the tenant context arguments is set back if the
    job fails for any reason.
    
    This should ensure that any retry of the job will be executed in the correct
    tenant context.
    
    https://appsignal.com/csa-admin/sites/672e0b61d2a5e4b5197f10e4/exceptions/incidents/252/samples/timestamp/2024-11-16T12:05:24Z

--- app/jobs/concerns/tenant_switcher.rb
+++ app/jobs/concerns/tenant_switcher.rb
@@ -5,26 +5,44 @@ module TenantSwitcher
 
   included do
     around_perform do |job, block|
-      Tenant.switch(@tenant) do
-        current_attrs = job.arguments.pop
-        Current.set(current_attrs, &block)
+      begin
+        context = job.arguments.pop
+        Tenant.switch(context["tenant"]) do
+          Current.set(context["current"], &block)
+        end
+      ensure
+        job.set_context(context)
       end
     end
   end
 
-  private
+  def serialize
+    set_context(
+      "tenant" => Tenant.current,
+      "current" => Current.attributes)
 
-  def serialize_arguments(arguments)
-    arguments << Current.attributes
-    arguments << Tenant.current
-    # We need to set this instance to avoid serializing multiple times,
-    # and then adding the extra arguments each time.
-    # See: https://github.com/rails/rails/blob/main/activejob/lib/active_job/core.rb#L175
-    @serialized_arguments = super(arguments)
+    super
   end
 
+  def set_context(context)
+    return if context_set?(context)
+
+    self.arguments << context
+  end
+
+  private
+
+  def context_set?(context)
+    last_argument = self.arguments&.last
+    return unless last_argument.is_a?(Hash)
+
+    last_argument.keys.sort == context.keys.sort
+  end
+
+  # Argument deserialization is done before the around_perform block is executed,
+  # so we need to ensure that the tenant context is set when deserializing the arguments.
   def deserialize_arguments(serialized_args)
-    @tenant = serialized_args.pop
-    Tenant.switch(@tenant) { super(serialized_args) }
+    context = serialized_args.last
+    Tenant.switch(context["tenant"]) { super }
   end
 end
