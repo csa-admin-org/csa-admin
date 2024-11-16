fa3fd603caeae9973a7554b0400b03b291ab581e
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2024-11-16 12:46:39 +0100

    ActiveJob TenantSwitcher improvements
    This patch switches the TenantSwitcher to store store the current attributes
    and tenant as last arguments in the jobs instead of storing them in custom
    job data.
    
    This should better ensure that those custom arguments are kept around when a
    job is retried.
    
    This should avoid such issue:
    https://appsignal.com/csa-admin/sites/672e0b61d2a5e4b5197f10e4/exceptions/incidents/252

---
+++ spec/jobs/concerns/tenant_switcher_spec.rb
@@ -0,0 +1,68 @@
+require "rails_helper"
+
+describe TenantSwitcher do
+  specify "add current attributes and tenant last arguments" do
+    class DummyJob < ActiveJob::Base
+      include TenantSwitcher
+      def perform(admin, name: nil)
+        admin.update!(name: name)
+      end
+    end
+
+    admin = create(:admin, name: "Admin")
+    Current.session = create(:session, id: 42)
+    DummyJob.perform_later(admin, name: "Admin!")
+
+    expect(enqueued_jobs.size).to eq(1)
+    job = enqueued_jobs.first
+    expect(job["arguments"]).to eq([
+      { "_aj_globalid" => "gid://csa-admin/Admin/1" },
+      { "name" => "Admin!", "_aj_ruby2_keywords" => [ "name" ] },
+      { "session" => { "_aj_globalid" => "gid://csa-admin/Session/42" }, "_aj_symbol_keys" => [ "session" ] },
+      "acme"
+    ])
+
+    perform_enqueued_jobs
+    expect(enqueued_jobs.size).to eq(0)
+    expect(admin.reload.name).to eq("Admin!")
+  end
+
+  specify "retry with the same current attributes and tenant last arguments" do
+    class DummyExceptionJob < ActiveJob::Base
+      include TenantSwitcher
+      retry_on Exception, wait: :polynomially_longer, attempts: 2
+
+      def perform(foo)
+        raise Exception
+      end
+    end
+
+    Current.session = create(:session, id: 42)
+    DummyExceptionJob.perform_later("bar")
+
+    expect(enqueued_jobs.size).to eq(1)
+    job = enqueued_jobs.first
+    expect(job["arguments"]).to eq([
+      "bar",
+      { "session" => { "_aj_globalid" => "gid://csa-admin/Session/42" }, "_aj_symbol_keys" => [ "session" ] },
+      "acme"
+    ])
+
+    # rescue Exception one time
+    perform_enqueued_jobs
+
+    expect(enqueued_jobs.size).to eq(1)
+    job = enqueued_jobs.first
+    expect(job["arguments"]).to eq([
+      "bar",
+      { "session" => { "_aj_globalid" => "gid://csa-admin/Session/42" }, "_aj_symbol_keys" => [ "session" ] },
+      "acme"
+    ])
+
+    begin
+      perform_enqueued_jobs
+    rescue Exception
+    end
+    expect(enqueued_jobs.size).to eq(0)
+  end
+end
