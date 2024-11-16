6f6e34f7f058864cc3359cef462dade0e12ccb23
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2024-11-16 12:46:39 +0100

    ActiveJob TenantSwitcher improvements
    This patch switches the TenantSwitcher to store store the current attributes
    and tenant as last arguments in the jobs instead of storing them in custom
    job data.
    
    This should better ensure that those custom arguments are kept around when a
    job is retried.
    
    This should avoid such issue:
    https://appsignal.com/csa-admin/sites/672e0b61d2a5e4b5197f10e4/exceptions/incidents/252

--- app/jobs/concerns/tenant_switcher.rb
+++ app/jobs/concerns/tenant_switcher.rb
@@ -4,29 +4,27 @@ module TenantSwitcher
   extend ActiveSupport::Concern
 
   included do
-    around_perform do |_, block|
+    around_perform do |job, block|
       Tenant.switch(@tenant) do
-        attrs = ActiveJob::Arguments.deserialize(@current).to_h
-        Current.set(attrs, &block)
+        current_attrs = job.arguments.pop
+        Current.set(current_attrs, &block)
       end
     end
   end
 
-  def serialize
-    super.merge(
-      "tenant" => Tenant.current,
-      "current" => ActiveJob::Arguments.serialize(Current.attributes))
-  end
-
-  def deserialize(data)
-    @tenant = data["tenant"]
-    @current = data["current"]
-    super
-  end
-
   private
 
-  def deserialize_arguments(arguments)
-    Tenant.switch(@tenant) { super }
+  def serialize_arguments(arguments)
+    arguments << Current.attributes
+    arguments << Tenant.current
+    # We need to set this instance to avoid serializing multiple times,
+    # and then adding the extra arguments each time.
+    # See: https://github.com/rails/rails/blob/main/activejob/lib/active_job/core.rb#L175
+    @serialized_arguments = super(arguments)
+  end
+
+  def deserialize_arguments(serialized_args)
+    @tenant = serialized_args.pop
+    Tenant.switch(@tenant) { super(serialized_args) }
   end
 end
