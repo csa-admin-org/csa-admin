442f0b6f82baeecd82c54d6d6c8f42c935750bab
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2024-12-08 15:58:36 +0100

    Fine-tune tailwind config

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,6 +1,7 @@
 import { execSync } from 'child_process'
 
 const defaultTheme = require('tailwindcss/defaultTheme')
+
 const activeAdminPath = execSync('bundle show activeadmin', { encoding: 'utf-8' }).trim()
 const activeAdminPlugin = require(`${activeAdminPath}/plugin`)
 
@@ -8,18 +9,14 @@ export default {
   content: [
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*',
-    `./vendor/javascript/flowbite.js`,
-    `${activeAdminPath}/vendor/javascript/flowbite.js`,
-    `${activeAdminPath}/plugin.js`,
-    `${activeAdminPath}/app/views/**/*.{arb,erb,html,rb}`,
+    './app/views/**/*.{arb,erb,html,rb}',
     './app/admin/**/*.{arb,erb,html,rb}',
     './app/assets/**/*.css',
+    `./vendor/javascript/flowbite.js`,
     './config/initializers/active_admin.rb',
-    './app/views/active_admin/**/*.{arb,erb,html,rb}',
-    './app/views/admin/**/*.{arb,erb,html,rb}',
-    './app/views/layouts/active_admin*.{erb,html}',
-    './app/javascript/**/*.js'
+    `${activeAdminPath}/vendor/javascript/flowbite.js`,
+    `${activeAdminPath}/plugin.js`,
+    `${activeAdminPath}/app/views/**/*.{arb,erb,html,rb}`
   ],
   theme: {
     extend: {
@@ -31,6 +28,6 @@ export default {
   darkMode: "class",
   plugins: [
     require('@tailwindcss/forms'),
-    require(`${activeAdminPath}/plugin`)
+    activeAdminPlugin
   ]
 }
