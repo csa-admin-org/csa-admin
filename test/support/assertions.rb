b44a9bda2cfcbae2fc7fd7f23ca1511892f1c679
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2025-01-22 09:01:54 +0100

    Add minitest-difftastic
    https://github.com/marcoroth/minitest-difftastic

--- test/support/assertions.rb
+++ test/support/assertions.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 module Assertions
+  include Minitest::Assertions
   # Checks if an array contains a consecutive sequence of elements
   def assert_contains(full_array, *partial_sequence)
     partial_sequence.flatten!
-    msg ||= "Expected: [\n#{full_array.map(&:inspect).join(",\n")}\n] to contain the sequence [\n#{partial_sequence.map(&:inspect).join(",\n")}\n] consecutively."
+    msg = -> { diff(full_array, partial_sequence) }
     assert full_array.each_cons(partial_sequence.size).include?(partial_sequence), msg
   end
 end
