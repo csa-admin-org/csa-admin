0899b71646cb7f9a00a52476a46d08e1925209ba
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2025-02-09 21:41:59 +0100

    Member billing section, display current membership pricing details
    This patch aims to clarify how the current membership is priced and how much has already been invoiced and how much remains to be invoiced.
    
    This prevents the member to open a invoice PDF to understand what is or will be happening on its membership invoicing.

--- test/system/members/billing_test.rb
+++ test/system/members/billing_test.rb
@@ -10,10 +10,17 @@ class Members::BillingTest < ApplicationSystemTestCase
     visit "/billing"
 
     assert_text "Billing\n⤷ 1 open invoice"
-    assert_text "1 open invoice"
-    assert_text "01.04.24Invoice ##{invoice.id} (Annual fee) CHF 30.00"
-    assert_text "Amount remaining to be paidCHF 30.00"
+
+    assert_text "To payCHF 30.00"
+    assert_text "Annual feeCHF 30.00"
     assert_text "Payment intervalAnnual"
+
+    assert_text "Open invoices"
+    assert_text "01.04.24Invoice ##{invoice.id} (Annual fee) CHF 30.00"
+
+    assert_text "History"
+    assert_text "No invoice or payment at the moment."
+    assert_text "Payments with reference numbers are processed automatically overnight. Please contact us if one of your payments does not appear."
   end
 
   test "list invoices and payments history" do
@@ -26,11 +33,22 @@ class Members::BillingTest < ApplicationSystemTestCase
 
     visit "/billing"
 
-    assert_text "History"
+    assert_text "Billing\n⤷ View history"
+
     assert_text "CreditCHF 42.00"
+    assert_text "Annual feeCHF 30.00"
     assert_text "Payment intervalQuarterly"
+
+    assert_text "Membership 2024"
+    assert_text "Baskets10x 20.00 200.00"
+    assert_text "CHF 200.00"
+    assert_text "Already invoiced- 0.00"
+    assert_text "Remaining to invoiceCHF 200.00"
+
+    assert_text "History"
     assert_text "01.05.24Payment without reference-CHF 42.00"
     assert_text "02.04.24Payment of invoice #901871612-CHF 10.00"
     assert_text "01.04.24Invoice ##{invoice.id} (Other) CHF 10.00"
+    assert_text "Payments with reference numbers are processed automatically overnight. Please contact us if one of your payments does not appear."
   end
 end
