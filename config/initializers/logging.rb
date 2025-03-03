762fa3c40bb3bdd4df8bdbe2aa5d05f9e607b21f
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2025-03-03 21:57:41 +0100

    Skip appsignal logging on dev

--- config/initializers/logging.rb
+++ config/initializers/logging.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
-appsignal_logger = Appsignal::Logger.new("rails")
-appsignal_logger.broadcast_to(Rails.logger)
-Rails.logger = ActiveSupport::TaggedLogging.new(appsignal_logger)
+unless Rails.env.local?
+  appsignal_logger = Appsignal::Logger.new("rails")
+  appsignal_logger.broadcast_to(Rails.logger)
+  Rails.logger = ActiveSupport::TaggedLogging.new(appsignal_logger)
+end
