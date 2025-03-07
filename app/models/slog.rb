1755a9fecc7ab814f55bb3681bf14abea8644eee
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2025-03-07 13:40:32 +0100

    Improve params filtering in logs

--- app/models/slog.rb
+++ app/models/slog.rb
@@ -2,7 +2,18 @@
 
 module SLog
   def self.log(event, context = {})
-    context[:org] = Tenant.current
-    Rails.logger.info { "#{event} #{context.map { |k, v| "#{k}=#{v.inspect}" }.join(' ')}" }
+    context = {
+      event: event.to_s,
+      org: Tenant.current
+    }.merge(context)
+    filtered_context = filter_parameters(context)
+
+    Rails.logger.info filtered_context.map { |k, v| "#{k}=#{v.inspect}" }.join(" ")
+  end
+
+  def self.filter_parameters(params)
+    @filterer ||= ActiveSupport::ParameterFilter.new(
+      Rails.application.config.filter_parameters)
+    @filterer.filter(params)
   end
 end
--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -5,6 +5,22 @@
 # Configure parameters to be partially matched (e.g. passw matches password) and filtered from the log file.
 # Use this to limit dissemination of sensitive information.
 # See the ActiveSupport::ParameterFilter documentation for supported notations and behaviors.
-Rails.application.config.filter_parameters += [
-  :passw, :email, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn, :cvv, :cvc
+Rails.application.config.filter_parameters += %i[
+  passw
+  email
+  name
+  address
+  to
+  recipient
+  from
+  secret
+  token
+  _key
+  crypt
+  salt
+  certificate
+  otp
+  ssn
+  cvv
+  c
 ]
