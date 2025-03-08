785dbb384ea29e260514e96bc93d98391360fbfa
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2025-03-08 11:00:47 +0100

    Only enable Appsignal::Logger on production environment

--- config/initializers/logging.rb
+++ config/initializers/logging.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-unless Rails.env.local?
+if Rails.env.production?
   appsignal_logger = Appsignal::Logger.new("rails")
   appsignal_logger.broadcast_to(Rails.logger)
   Rails.logger = ActiveSupport::TaggedLogging.new(appsignal_logger)
