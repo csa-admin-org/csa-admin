8ef6b28c37c6d41875084b97b9eb32ce8d66c284
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2025-03-15 21:19:56 +0100

    Bring back flowbite patch for turbo

--- app/javascript/members.js
+++ app/javascript/members.js
@@ -3,3 +3,9 @@ import "@hotwired/turbo-rails"
 import "controllers/members"
 
 import "flowbite"
+// https://github.com/themesberg/flowbite/issues/88#issuecomment-1962238351
+window.document.addEventListener("turbo:submit-end", (_event) => {
+  window.setTimeout(() => {
+    window.initFlowbite()
+  }, 10)
+})
