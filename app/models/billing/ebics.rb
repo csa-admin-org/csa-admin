04405618cda60c098bf90a05ef2ccd706cb45e0b
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2025-05-29 12:06:10 +0200

    Add EBICS for tapatate

--- app/models/billing/ebics.rb
+++ app/models/billing/ebics.rb
@@ -6,12 +6,21 @@ module Billing
 
     def initialize(credentials = {})
       @credentials = credentials
-      @client = client
     end
 
     def payments_data
       files = get_camt54_files
       CamtFile.new(files).payments_data
+    end
+
+    def client
+      @client ||= Epics::Client.new(
+        @credentials.fetch(:keys),
+        @credentials.fetch(:secret),
+        @credentials.fetch(:url),
+        @credentials.fetch(:host_id),
+        @credentials.fetch(:participant_id),
+        @credentials.fetch(:client_id))
     end
 
     private
@@ -26,16 +35,6 @@ module Billing
       else
         raise e
       end
-    end
-
-    def client
-      Epics::Client.new(
-        @credentials.fetch(:keys),
-        @credentials.fetch(:secret),
-        @credentials.fetch(:url),
-        @credentials.fetch(:host_id),
-        @credentials.fetch(:participant_id),
-        @credentials.fetch(:client_id))
     end
   end
 end
