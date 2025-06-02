304f744a0efb661851e1d58cb0982d6863c84002
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2025-06-02 21:48:07 +0200

    Add validation to prevent zero-byte attachments
    Close https://appsignal.com/csa-admin/sites/672e0b61d2a5e4b5197f10e4/exceptions/incidents/467/samples/timestamp/2025-06-02T18:02:24Z

--- app/models/concerns/has_attachments.rb
+++ app/models/concerns/has_attachments.rb
@@ -11,6 +11,7 @@ module HasAttachments
     accepts_nested_attributes_for :attachments, allow_destroy: true
 
     validate :attachments_must_not_exceed_maximum_size
+    validate :attachments_must_not_be_zero_bytes
   end
 
   private
@@ -18,6 +19,14 @@ module HasAttachments
   def attachments_must_not_exceed_maximum_size
     if attachments.sum { |a| a.file.byte_size } > MAXIMUM_SIZE
       errors.add(:attachments, :too_large)
+    end
+  end
+
+  def attachments_must_not_be_zero_bytes
+    attachments.each do |attachment|
+      if attachment.file.byte_size.zero?
+        errors.add(:attachments, :zero_bytes)
+      end
     end
   end
 end
