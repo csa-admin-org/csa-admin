4a28aef86b5df4c3e2d6f34dfb165198acd0f6ae
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2025-06-27 09:19:19 +0200

    Add mailer job error handling
    Fix
    https://appsignal.com/csa-admin/sites/672e0b61d2a5e4b5197f10e4/exceptions/incidents/481/samples/timestamp/2025-06-26T08:10:57Z
    https://appsignal.com/csa-admin/sites/672e0b61d2a5e4b5197f10e4/exceptions/incidents/482/samples/timestamp/2025-06-26T08:10:57Z

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.config.after_initialize do
+  ActionMailer::MailDeliveryJob.include TenantContext
   ActionMailer::MailDeliveryJob.retry_on Postmark::TimeoutError
+  ActionMailer::MailDeliveryJob.discard_on ActiveJob::DeserializationError
 end
