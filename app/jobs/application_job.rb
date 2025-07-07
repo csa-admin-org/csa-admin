a6bbaeb76a8cff97fa2f42e8a69303d25d77a387
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2025-07-07 17:16:26 +0200

    Add retry for ActiveStorage FileNotFoundError
    Fix
    https://appsignal.com/csa-admin/sites/672e0b61d2a5e4b5197f10e4/exceptions/incidents/483/samples/timestamp/2025-07-07T11:48:50Z

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -5,6 +5,7 @@ class ApplicationJob < ActiveJob::Base
 
   # Automatically retry jobs that encountered a deadlock
   retry_on ActiveRecord::Deadlocked
+  retry_on ActiveStorage::FileNotFoundError
 
   # Most jobs are safe to ignore if the underlying records are no longer available
   discard_on ActiveJob::DeserializationError
