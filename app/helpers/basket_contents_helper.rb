0cb55cdca17bdff7a2b159ab541c6dbafa095caf
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2025-07-21 01:08:48 +0200

    Add safe navigation operators to handle nil delivery
    Fix
    https://appsignal.com/csa-admin/sites/672e0b61d2a5e4b5197f10e4/exceptions/incidents/486/samples/timestamp/2025-07-20T19:31:37Z

--- app/helpers/basket_contents_helper.rb
+++ app/helpers/basket_contents_helper.rb
@@ -7,7 +7,7 @@ module BasketContentsHelper
   def smart_basket_contents_path
     @delivery ||= BasketContent.closest_delivery || Delivery.next || Delivery.last
     if @delivery
-      basket_contents_path(q: { delivery_id_eq: @delivery.id, during_year: @delivery.fy_year })
+      basket_contents_path(q: { delivery_id_eq: @delivery&.id, during_year: @delivery&.fy_year })
     else
       basket_contents_path
     end
