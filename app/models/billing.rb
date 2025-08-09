910ac1a67b1fe866038be705f88ffe9d48ed586b
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2025-08-09 10:47:25 +0200

    Add support for Netherlands (NL) as SEPA country
    The above changes add support for Netherlands IBAN format and cleanup country
    code checks by introducing a `swiss_qr?` predicate method along with expanding
    `sepa?` to include both Germany and Netherlands.

--- app/models/billing.rb
+++ app/models/billing.rb
@@ -4,29 +4,25 @@ module Billing
   extend self
 
   def iban_format(country_code = nil)
-    country_code ||= Current.org.country_code
-    case country_code
+    case Current.org.country_code
     when "CH"; /\ACH\d{2}3[01]\d{3}[a-z0-9]{12}\z/i # QR IBAN
     when "FR"; /\AFR\d{12}[a-z0-9]{11}\d{2}\z/i
     when "DE"; /\ADE\d{20}\z/i
+    when "NL"; /\ANL\d{2}[A-Z]{4}\d{10}\z/i
     end
   end
 
   def iban_placeholder(country_code = nil)
-    country_code ||= Current.org.country_code
-    case country_code
+    case Current.org.country_code
     when "CH"; "CHXX 3XXX XXXX XXXX XXXX X"
     when "FR"; "FRXX XXXX XXXX XXXX XXXX XXXX XXX"
     when "DE"; "DEXX XXXX XXXX XXXX XXXX XX"
+    when "NL"; "NLXX XXXX XXXX XXXX XX"
     end
   end
 
   def reference(country_code = nil)
-    country_code ||= Current.org.country_code
-    case country_code
-    when "CH"; SwissQRReference
-    else ScorReference
-    end
+    Current.org.swiss_qr? ? SwissQRReference : ScorReference
   end
 
   def import_payments(file)
