21fd455acbb6f99d14ca71fe06a5c2b9b5682779
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2025-10-02 15:36:51 +0200

    Add LogSubscriber to emit Rails events to Rails logger

--- config/initializers/logging.rb
+++ config/initializers/logging.rb
@@ -5,3 +5,16 @@ if Rails.env.production?
   appsignal_logger.broadcast_to(Rails.logger)
   Rails.logger = ActiveSupport::TaggedLogging.new(appsignal_logger)
 end
+
+class LogSubscriber
+  def emit(event)
+    payload = {}
+    payload.merge!(event[:payload] || {})
+    payload.merge!(event[:tags] || {})
+    payload.merge!(event[:context] || {})
+    payload = payload.map { |key, value| "#{key}=#{value}" }.join(" ")
+    Rails.logger.info("event=#{event[:name]} #{payload}")
+  end
+end
+
+Rails.event.subscribe(LogSubscriber.new)
