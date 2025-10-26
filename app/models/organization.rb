fd4b4fb6aae436bdba258c4f79f3e0dd3ea8fdaf
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2025-10-26 09:45:49 +0100

    Move billing logic to Organization::Billing

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -14,9 +14,6 @@ class Organization < ApplicationRecord
   RESTRICTED_FEATURES = %i[]
   FEATURE_FLAGS = %i[]
   LANGUAGES = %w[fr de it nl en]
-  CURRENCIES = %w[CHF EUR]
-  BILLING_YEAR_DIVISIONS = [ 1, 2, 3, 4, 12 ]
-  BANK_CONNECTION_TYPES = %w[ebics bas mock]
   MEMBER_FORM_MODES = %w[membership shop]
   INPUT_FORM_MODES = %w[hidden visible required]
   DELIVERY_PDF_MEMBER_INFOS = %w[none phones food_note]
@@ -29,7 +26,6 @@ class Organization < ApplicationRecord
   def self.feature_flags = FEATURE_FLAGS
   def self.restricted_features = RESTRICTED_FEATURES
   def self.languages = LANGUAGES
-  def self.billing_year_divisions = BILLING_YEAR_DIVISIONS
 
   include HasSocialNetworkUrls
   include TranslatedAttributes
@@ -37,6 +33,7 @@ class Organization < ApplicationRecord
   include NormalizedString
   include HasIBAN
   include AbsenceFeature, ActivityFeature, BasketPriceExtraFeature, NewMemberFeeFeature, ShopFeature
+  include Organization::Billing
 
   attribute :icalendar_auth_token, :string, default: -> { SecureRandom.hex(16) }
 
@@ -69,23 +66,7 @@ class Organization < ApplicationRecord
   validates :email_default_from, format: { with: ->(org) { /.*@#{org.domain}\z/ } }
   validates_plausible_phone :phone, country_code: ->(org) { org.country_code }
   validates_plausible_phone :activity_phone, country_code: ->(org) { org.country_code }
-  validates :creditor_name, :creditor_address, :creditor_city, :creditor_zip, presence: true
-  validates :bank_reference, format: { with: /\A\d+\z/, allow_blank: true }
-  validates :iban, format: ->(org) { Billing.iban_format(org.country_code) }, allow_nil: true, if: :country_code?
-  validates :fiscal_year_start_month,
-    presence: true,
-    inclusion: { in: 1..12 }
-  validates_with SEPA::CreditorIdentifierValidator, field_name: :sepa_creditor_identifier, if: :sepa_creditor_identifier?
-  validates :billing_year_divisions, presence: true
-  validates :bank_connection_type, inclusion: { in: BANK_CONNECTION_TYPES }, allow_nil: true
-  validates :bank_credentials, presence: true, if: :bank_connection_type?
   validates :trial_baskets_count, numericality: { greater_than_or_equal_to: 0 }, presence: true
-  validates :annual_fee, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
-  validates :annual_fee_member_form, absence: true, unless: :annual_fee?
-  validates :share_price, numericality: { greater_than_or_equal_to: 1 }, allow_nil: true
-  validates :share_price, presence: true, if: :shares_number?
-  validates :shares_number, numericality: { greater_than_or_equal_to: 1 }, allow_nil: true
-  validates :shares_number, presence: true, if: :share_price?
   validates :open_renewal_reminder_sent_after_in_days,
     numericality: { greater_than_or_equal_to: 1, allow_nil: true }
   validates :bidding_round_basket_size_price_min_percentage,
@@ -96,16 +77,9 @@ class Organization < ApplicationRecord
     presence: true
   validates :open_bidding_round_reminder_sent_after_in_days,
     numericality: { greater_than_or_equal_to: 1, allow_nil: true }
-  validates :vat_membership_rate, numericality: { greater_or_equal_to_than: 0, allow_nil: true }
-  validates :vat_activity_rate, numericality: { greater_or_equal_to_than: 0, allow_nil: true }
-  validates :vat_number, presence: true, if: -> {
-    vat_membership_rate&.positive? || vat_activity_rate&.positive? || vat_shop_rate&.positive?
-  }
-  validates :recurring_billing_wday, inclusion: { in: 0..6 }, allow_nil: true
   validates :country_code,
     presence: true,
     inclusion: { in: ISO3166::Country.all.map(&:alpha2) }
-  validates :currency_code, presence: true, inclusion: { in: CURRENCIES }
   validates :member_form_mode, presence: true, inclusion: { in: MEMBER_FORM_MODES }
   validates :member_profession_form_mode, presence: true, inclusion: { in: INPUT_FORM_MODES }
   validates :member_come_from_form_mode, presence: true, inclusion: { in: INPUT_FORM_MODES }
@@ -129,7 +103,6 @@ class Organization < ApplicationRecord
     numericality: { greater_than_or_equal_to: 0 }
   validate :only_one_organization, on: :create
 
-  after_save :apply_annual_fee_change
   before_create :set_defaults
   after_create :create_default_configurations
 
@@ -161,33 +134,6 @@ class Organization < ApplicationRecord
     trial_baskets_count.positive?
   end
 
-  def recurring_billing?
-    !!recurring_billing_wday
-  end
-
-  def bank_connection?
-    bank_connection_type?
-  end
-
-  def bank_connection
-    case bank_connection_type
-    when "ebics"
-      Billing::EBICS.new(bank_credentials)
-    when "bas"
-      Billing::BAS.new(bank_credentials)
-    when "mock"
-      Billing::EBICSMock.new(bank_credentials)
-    end
-  end
-
-  def send_invoice_overdue_notice?
-    bank_connection? && MailTemplate.active_template("invoice_overdue_notice")
-  end
-
-  def billing_year_divisions=(divisions)
-    super divisions.map(&:presence).compact.map(&:to_i) & BILLING_YEAR_DIVISIONS
-  end
-
   def phone=(phone)
     super PhonyRails.normalize_number(phone, default_country_code: country_code)
   end
@@ -197,11 +143,11 @@ class Organization < ApplicationRecord
   end
 
   def languages=(languages)
-    super languages & self.class.languages
+    super languages & LANGUAGES
   end
 
   def languages
-    super & self.class.languages
+    super & LANGUAGES
   end
 
   def default_locale
@@ -218,14 +164,6 @@ class Organization < ApplicationRecord
 
   def time_zone
     country.timezones.zone_info.first.identifier
-  end
-
-  def swiss_qr?
-    country_code == "CH"
-  end
-
-  def sepa?
-    country_code.in? %w[ DE NL ]
   end
 
   def email_default_from_address
@@ -260,56 +198,6 @@ class Organization < ApplicationRecord
       admin_url,
       members_url
     ].map { |url| URI.parse(url).host }
-  end
-
-  def fiscal_years
-    min_year = Delivery.minimum(:date)&.year || Date.current.year
-    max_year = Delivery.maximum(:date)&.year || Date.current.year
-    (min_year..max_year).map { |year|
-      Current.org.fiscal_year_for(year)
-    }
-  end
-
-  def current_fiscal_year
-    FiscalYear.current(start_month: fiscal_year_start_month)
-  end
-
-  def last_fiscal_year
-    fiscal_year_for(current_fiscal_year.year - 1)
-  end
-
-  def next_fiscal_year
-    fiscal_year_for(current_fiscal_year.year + 1)
-  end
-
-  def fiscal_year_for(date_or_year)
-    return unless date_or_year
-
-    FiscalYear.for(date_or_year, start_month: fiscal_year_start_month)
-  end
-
-  def fy_month_for(date)
-    fiscal_year_for(date).month(date)
-  end
-
-  def member_support?
-    annual_fee? || share?
-  end
-
-  def annual_fee?
-    annual_fee && annual_fee >= 0
-  end
-
-  def share?
-    share_price&.positive?
-  end
-
-  def deliveries_count(year)
-    @max_deliveries_counts ||=
-      DeliveryCycle
-        .pluck(:deliveries_counts)
-        .reduce({}) { |h, i| h.merge(i) { |k, old, new| [ old, new ].flatten.max } }
-    @max_deliveries_counts[year.to_s]
   end
 
   private
@@ -361,14 +249,6 @@ class Organization < ApplicationRecord
     DeliveryCycle.create_default!
     MailTemplate.create_all!
     Newsletter::Template.create_defaults!
-  end
-
-  def apply_annual_fee_change
-    return unless annual_fee_previously_changed?
-
-    Member
-      .where(annual_fee: annual_fee_previously_was)
-      .update_all(annual_fee: annual_fee)
   end
 end
 
