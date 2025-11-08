68e07b2b2d0f4087b537cadd880e922217a1ca5f
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2025-11-08 10:26:35 +0100

    Add gibberish detection to SpamDetector
    Detect gibberish in name/address/city/zip/come_from/note by checking letter
    consonant ratio and minimum length. Remove old numeric zip regexp check and add
    tests for gibberish and normal cases.

--- app/models/spam_detector.rb
+++ app/models/spam_detector.rb
@@ -4,9 +4,11 @@ require "cld"
 
 class SpamDetector
   MAX_SIZE = 5000
-  ZIP_REGEXP = /\A\d{6}\z/
   CYRILLIC_CHECK = /\p{Cyrillic}+/ui
   TEXTS_COLUMNS = %w[note food_note come_from].freeze
+  GIBBERISH_FIELDS = %w[name address city zip come_from note].freeze
+  GIBBERISH_MIN_LENGTH = 20
+  GIBBERISH_RATIO = 0.74
 
   def self.spam?(member)
     new(member).spam?
@@ -25,13 +27,13 @@ class SpamDetector
   def spam?
     @member.note.to_s.size > MAX_SIZE ||
       @member.food_note.to_s.size > MAX_SIZE ||
-      @member.zip&.match?(ZIP_REGEXP) ||
       @member.address&.match?(CYRILLIC_CHECK) ||
       @member.city&.match?(CYRILLIC_CHECK) ||
       @member.come_from&.match?(CYRILLIC_CHECK) ||
       non_native_language? ||
       long_duplicated_texts? ||
-      non_allowed_country?
+      non_allowed_country? ||
+      gibberish?
   end
 
   def non_allowed_country?
@@ -45,7 +47,7 @@ class SpamDetector
     languages = I18n.available_locales.map(&:to_s)
     languages << "un" # Unknown CLD language
     TEXTS_COLUMNS.any? { |attr|
-      text = @member.send(attr).dup
+      text = @member.send(attr)
       if text && text.size > 100
         cld = CLD.detect_language(text)
         cld[:reliable] && languages.exclude?(cld[:code])
@@ -55,11 +57,26 @@ class SpamDetector
 
   def long_duplicated_texts?
     texts = TEXTS_COLUMNS.map { |attr|
-      text = @member.send(attr).dup
+      text = @member.send(attr)
       if text.present? && text.size > 40
-        text.gsub!(/\s/, "")
+        text.gsub(/\s/, "")
       end
     }.compact
     texts.any? { |t| texts.count(t) > 1 }
+  end
+
+  def gibberish?
+    GIBBERISH_FIELDS.any? do |field|
+      text = @member.send(field)
+      next if text.blank? || text.length < GIBBERISH_MIN_LENGTH || text =~ /[^\p{L}]/
+
+      letters = text.scan(/\p{L}/)
+      consonants = letters.count { |c| !/[aeiou]/i.match?(c) }
+      total_letters = letters.length
+      next if total_letters < 5
+
+      ratio = consonants.to_f / total_letters
+      ratio > GIBBERISH_RATIO
+    end
   end
 end
