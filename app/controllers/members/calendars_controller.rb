b2b09b2d0c66db7c775c3c534800fbe6c5ecef29
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2025-11-23 14:35:38 +0100

    Add updated_at indexes and optimize member calendar
    Add DB indexes on updated_at and on [membership_id/member_id, updated_at] for
    baskets and activity_participations to speed cache checks Refactor
    Members::CalendarsController to compute last_modified by calling maximum on the
    relations, delay eager-loading until after fresh_when, and shorten period_range
    to 6 months ago through the next fiscal year end

--- app/controllers/members/calendars_controller.rb
+++ app/controllers/members/calendars_controller.rb
@@ -3,31 +3,26 @@
 class Members::CalendarsController < Members::BaseController
   # GET /deliveries/calendar.ics
   def show
-    @baskets =
-      @member
-        .baskets
-        .filled
-        .between(period_range)
-        .includes(:basket_size, :depot, baskets_basket_complements: :basket_complement)
-    participations =
-      @member
-        .activity_participations
-        .between(period_range)
-        .includes(:activity)
-    @activity_participations = ActivityParticipationGroup.group(participations)
+    @baskets = @member.baskets.filled.between(period_range)
+    @participations = @member.activity_participations.between(period_range)
 
     last_changed = [
-      @baskets.maximum(:updated_at),
-      participations.maximum(:updated_at)
-    ].compact.max
+      @baskets,
+      @participations
+    ].map { |rel| rel.maximum(:updated_at) }.compact.max
     fresh_when last_modified: last_changed
+
+    @baskets =
+      @baskets.includes(:basket_size, :depot,
+        baskets_basket_complements: :basket_complement)
+    @activity_participations =
+      ActivityParticipationGroup.group(@participations.includes(:activity))
   end
 
   private
 
   def period_range
-    fy = Current.fiscal_year
-    (fy.beginning_of_year - 1.year)..(fy.end_of_year + 1.year)
+    6.months.ago..(Current.fiscal_year.end_of_year + 1.year)
   end
 
   def current_member
