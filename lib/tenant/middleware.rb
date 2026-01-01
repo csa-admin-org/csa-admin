2dc81ff9ddcb841083e28477c9dfa792e0841a8c
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2026-01-01 16:00:11 +0100

    Prioritize tenant host lookup in middleware

--- lib/tenant/middleware.rb
+++ lib/tenant/middleware.rb
@@ -11,10 +11,10 @@ module Tenant
       request = ActionDispatch::Request.new(env)
       if request.path == "/up"
         @app.call(env)
-      elsif PublicSuffix.parse(request.host).domain == ENV["APP_DOMAIN"]
-        @app.call(env)
       elsif tenant = Tenant.find_by(host: request.host)
         Tenant.switch(tenant) { @app.call(env) }
+      elsif PublicSuffix.parse(request.host).domain == ENV["APP_DOMAIN"]
+        @app.call(env)
       else
         [ 404, {}, [ "Not Found" ] ]
       end
