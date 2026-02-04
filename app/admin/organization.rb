d2c6cfeb81f812cac3c6a281387b004023bb2ad1
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2026-02-04 16:12:53 +0100

    Show QR-IBAN hint in organization form

--- app/admin/organization.rb
+++ app/admin/organization.rb
@@ -75,6 +75,7 @@ ActiveAdmin.register Organization do
           f.input :iban,
             label: f.object.swiss_qr? ? "QR-IBAN" : "IBAN",
             placeholder: Billing.iban_placeholder(f.object.country_code),
+            hint: (t("formtastic.hints.organization.iban_html") if f.object.swiss_qr?),
             input_html: { value: f.object.iban_formatted }
           if f.object.sepa?
             f.input :sepa_creditor_identifier, input_html: { maxlength: 35, placeholder: sepa_creditor_identifier_placeholder }
