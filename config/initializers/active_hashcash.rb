c56d07ec08f76fc6477863225dd5b36a932ee3b5
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2026-03-22 10:24:41 +0100

    Lower hashcash bits in non-production for faster development
    Set bits to 1 in development/test environments to reduce computational overhead,
    while maintaining 18 in production for security. Also configure base controller
    class.

--- config/initializers/active_hashcash.rb
+++ config/initializers/active_hashcash.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
-ActiveHashcash.bits = 18 # Below default 20, but safe given allow_browser minimum versions
 ActiveHashcash.base_controller_class = "ApplicationController"
+
+# Below default 20, but safe given allow_browser minimum versions
+ActiveHashcash.bits = Rails.env.production? ? 18 : 1
